c83e5f9ac36cc9fe159d53357d7aaf046f8a8ce8
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-23 11:54:06 +0200

    [dir]{layouts}=> rename to layout

---
+++ resources/views/layout/basic.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- meta tags start -->
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- meta tags end -->
+
+    <!-- style start -->
+    <!-- style end -->
+
+    <!-- script start -->
+    <!-- script end -->
+
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- javascript footer start -->
+    <!-- javascript footer end -->
+</body>
+</html>
