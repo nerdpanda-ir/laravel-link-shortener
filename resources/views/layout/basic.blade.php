2d457e371aabc667afc321f7d9cf6229f4fb1f1c
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 14:17:06 +0200

    [file]{basic.blade.php}=> pass $htmlAttributes  to header component

--- resources/views/layout/basic.blade.php
+++ resources/views/layout/basic.blade.php
@@ -2,6 +2,6 @@
     $htmlAttributes = \ComponentAttributeBagFactoryProxyFacade::create($htmlAttributes??null);
     $bodyAttributes = \ComponentAttributeBagFactoryProxyFacade::create($bodyAttributes??null);
 @endphp
-<x-partial.layout.basic.header />
+<x-partial.layout.basic.header :html-attributes="$htmlAttributes"/>
 <x-partial.layout.basic.body />
 <x-partial.layout.basic.footer />
