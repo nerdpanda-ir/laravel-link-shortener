71fce485d4c878dc611d5a5b722375f741375b94
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 18:23:22 +0200

    [file]{home.blade.php}-> modify home

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,2 +1,12 @@
-@extends('page.layouts.bootstrap')
-
+@extends('layouts.bootstrap')
+@section('title')
+    Home
+@endsection
+@section('content')
+<x-partials.nav />
+<main class="container py-5">
+    <h1>Link Shortener</h1>
+    <p>you can short your links with me !!!</p>
+    <hr class="my-5">
+</main>
+@endsection
