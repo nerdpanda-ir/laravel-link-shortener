43d8bbea978a0d3e544d72b6e8d7364cb51ce5eb
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 06:33:42 +0200

    [file]{header.blade.php}=> create file

---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.basic' , ['htmlAttrs'=>['lang'=>'en']  , 'charset' => 'gg'])
+@section('content')
+    <section>
+        @section('gg')
+
+        @show
+    </section>
+@endsection
