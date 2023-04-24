13bab891be163a8b5961ae0e26db5bcf0521a609
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 17:52:31 +0200

    [dir]{bootstrap.blade.php}-> create bootstrap ;

---
+++ resources/views/page/layouts/bootstrap.blade.php
@@ -0,0 +1,7 @@
+@extends('MasterBlade::layout.basic')
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/lib/bootstrap.css')}}">
+@endsection
+@section('footer-javascript')
+<script src="{{asset('js/lib/bootstrap.bundle.js')}}"></script>
+@endsection
