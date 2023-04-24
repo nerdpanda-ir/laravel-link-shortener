52b7b78b51a323abc14b791f443ba1b11f1c7910
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 06:39:39 +0200

    [file]{home.blade.php}=> extend basic layout

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,8 +1,1 @@
-@extends('layout.basic' , ['htmlAttrs'=>['lang'=>'en']  , 'charset' => 'gg'])
-@section('content')
-    <section>
-        @section('gg')
-
-        @show
-    </section>
-@endsection
+@extends('layout.basic')
