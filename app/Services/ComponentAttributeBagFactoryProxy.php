d277b9f9aa2faceb0cba67e352a0945eb78cfbc7
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 09:01:47 +0200

    [file]{ComponentAttributeBagFactoryProxy.php}=> implement from contract

--- app/Services/ComponentAttributeBagFactoryProxy.php
+++ app/Services/ComponentAttributeBagFactoryProxy.php
@@ -2,9 +2,10 @@
 
 namespace App\Services;
 
+use App\Contracts\ComponentAttributeBagFactoryProxyContract;
 use Illuminate\View\ComponentAttributeBag;
 
-class ComponentAttributeBagFactoryProxy
+class ComponentAttributeBagFactoryProxy implements ComponentAttributeBagFactoryProxyContract
 {
     public function create(?array $attributes):ComponentAttributeBag|null {
         if (is_null($attributes))
