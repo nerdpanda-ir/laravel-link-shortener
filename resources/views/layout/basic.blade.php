a1b5e1157ff064e75cbffb697d78b07e78a4d1a2
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 16:18:30 +0200

    [file]{basic.blade.php}=> define $charset and pass to header component

--- resources/views/layout/basic.blade.php
+++ resources/views/layout/basic.blade.php
@@ -1,7 +1,8 @@
 @php
     $htmlAttributes = \ComponentAttributeBagFactoryProxyFacade::create($htmlAttributes??null);
     $bodyAttributes = \ComponentAttributeBagFactoryProxyFacade::create($bodyAttributes??null);
+    $charset = $charset ?? 'UTF-8';
 @endphp
-<x-partial.layout.basic.header :html-attributes="$htmlAttributes"/>
+<x-partial.layout.basic.header :html-attributes="$htmlAttributes" :charset="$charset"/>
 <x-partial.layout.basic.body :body-attributes="$bodyAttributes"/>
 <x-partial.layout.basic.footer />
