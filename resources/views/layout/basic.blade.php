7f3c6589635b55b7cf17e8fda6778e98b6624f14
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 14:16:11 +0200

    [file]{basic.blade.php}=> define body attributes and html attributes

--- resources/views/layout/basic.blade.php
+++ resources/views/layout/basic.blade.php
@@ -1,3 +1,7 @@
+@php
+    $htmlAttributes = \ComponentAttributeBagFactoryProxyFacade::create($htmlAttributes??null);
+    $bodyAttributes = \ComponentAttributeBagFactoryProxyFacade::create($bodyAttributes??null);
+@endphp
 <x-partial.layout.basic.header />
 <x-partial.layout.basic.body />
 <x-partial.layout.basic.footer />
