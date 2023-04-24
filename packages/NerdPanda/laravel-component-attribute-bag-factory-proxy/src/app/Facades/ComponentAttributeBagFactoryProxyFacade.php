5aed1ffe857cdf468295d123be0afac7c2517cb0
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-24 14:08:21 +0200

    [file]{ComponentAttributeBagFactoryProxyFacade.php}=> modify getFacadeAccessor method

--- packages/NerdPanda/laravel-component-attribute-bag-factory-proxy/src/app/Facades/ComponentAttributeBagFactoryProxyFacade.php
+++ packages/NerdPanda/laravel-component-attribute-bag-factory-proxy/src/app/Facades/ComponentAttributeBagFactoryProxyFacade.php
@@ -4,12 +4,14 @@ namespace NerdPanda\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\View\ComponentAttributeBag;
+use NerdPanda\Providers\ComponentAttributeBagFactoryProxyProvider;
 
 /** @method ComponentAttributeBag|null create(null|array $attributes)*/
 class ComponentAttributeBagFactoryProxyFacade extends Facade
 {
     protected static function getFacadeAccessor():string
     {
+        app()->loadDeferredProvider(ComponentAttributeBagFactoryProxyProvider::class);
         return config('componentAttributeBagFactoryProxy.alias');
     }
 }
