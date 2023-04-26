d37b725967117552feda03ee5d80193b4e94149f
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-26 14:33:52 +0200

    [file]{2023_04_26_123120_create_permissions_table.php}-> create permission migration

---
+++ database/migrations/2023_04_26_123120_create_permissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',64)->unique();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by')
+                ->on('users')->references('id')
+                ->onDelete('set null')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
