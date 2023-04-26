58502df6ea75370716b0f8bc31b288f4e6fd8292
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-26 13:27:51 +0200

    [file]{2023_04_26_111113_add_user_id_to_users_table.php.php}-> modify

--- database/migrations/2023_04_26_111113_add_user_id_to_users_table.php
+++ database/migrations/2023_04_26_111113_add_user_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_id',42)->unique();
+            $table->string('user_id',42)->unique()->after('email');
         });
     }
 
