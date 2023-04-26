8a580a3bd256ee4f0d8bbcecbfc3eb0fdaa2e4eb
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-26 13:29:19 +0200

    [file]{UserSeeder.php}=> create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Contracts\UserFactoryContract;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    protected static int $count = 15;
+    /**
+     * Run the database seeds.
+     */
+    public function run(UserFactoryContract $factory): void
+    {
+        $factory->modelName()::truncate();
+        $factory->nerdPanda()->create();
+        $factory->count(self::$count)->create();
+    }
+}
