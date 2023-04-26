a25e2ab9af7bca33c233ffc281c4ebd38ff97560
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-26 14:30:36 +0200

    [file]{app.php}=> register provider to application

--- config/app.php
+++ config/app.php
@@ -168,7 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \App\Providers\UserFactoryServiceProvider::class
+        \App\Providers\UserFactoryServiceProvider::class ,
+        \App\Providers\UserableServiceProvider::class
     ])->toArray(),
 
     /*
