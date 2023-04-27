8561de16f2f9993c5c1ed9fbb91c1219a2f0e4b2
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-27 09:38:09 +0200

    [file]{VerifiedEmailScopeTrait.php}=> rename method

--- packages/NerdPanda/laravel-verified-email-scope/src/app/Traits/Model/VerifiedEmailScopeTrait.php
+++ packages/NerdPanda/laravel-verified-email-scope/src/app/Traits/Model/VerifiedEmailScopeTrait.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Database\Query\Builder;
 
 trait VerifiedEmailScopeTrait
 {
-    public function scopeVerifiedEmail(Builder $query , string $column='email_verified_at') {
+    public function scopeWhereVerifiedEmail(Builder $query , string $column='email_verified_at') {
         $query->whereNotNull($column);
     }
 }
