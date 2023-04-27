37b34598f2920c0a957be180e0891c33d11ba92b
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-27 09:08:31 +0200

    [file]{database/seeders/PermissionSeeder.php}=> create seeding script

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Contracts\PermissionFactoryContract;
+use App\Contracts\UserableContract;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class PermissionSeeder extends Seeder
@@ -10,8 +14,22 @@ class PermissionSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(UserableContract $user , PermissionFactoryContract $factory): void
     {
-        //
+        $factory->newModel()->truncate();
+        /** @var Collection $users */
+        $users = $user->isVerifiedEmail()->get(['id']);
+        if (empty($users))
+            throw new \Exception("no found any user for permission seeding ");
+        $permissions = [
+            'create-user','update-user', 'delete-user' ,'force-delete-user'
+            ,'see-system-monitor' , 'see-application-setting',
+            'update-application-setting',
+        ];
+        foreach ($permissions as $permission)
+            $factory->create([
+                'name'=>$permission,'created_by'=> $users->random()
+            ]);
+
     }
 }
