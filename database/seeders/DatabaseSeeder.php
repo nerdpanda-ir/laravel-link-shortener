d2b050f36f7f61bb49f561165dfc2b75f86c613a
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-27 08:42:17 +0200

    [file]{DatabaseSeeder.php}=> modify class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Contracts\PermissionFactoryContract;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,8 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(PermissionFactoryContract $factory): void
     {
+        // @todo seeders can be resolve with interface
+        $this->call([
+            UserSeeder::class ,
+            PermissionSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
