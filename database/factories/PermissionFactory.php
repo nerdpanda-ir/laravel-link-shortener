fd8b7a87d40c5b336da05ff684e31824e6d9fbd2
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-28 11:36:00 +0200

    [file]{PermissionFactory.php}-> remove additional methods

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Contracts\PermissionFactoryContract;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Permission>
  */
@@ -16,26 +18,15 @@ class PermissionFactory extends Factory implements PermissionFactoryContract
      */
     public function definition(): array
     {
-        $createdAt = $this->get_created_at( end: now()->subHours(3)->toDateTime());
-        $forwardCreatedAt = $this->forwardDateTime($createdAt);
-        $updatedAt = $this->get_updated_at($forwardCreatedAt);
+        $createdAt = $this->faker->dateTimeBetween('-2 years',now()->subHours(2));
+        $forwardCreatedAt = Carbon::createFromTimestamp($createdAt->getTimestamp())
+                            ->addSeconds(rand(85,3600));
+        $updatedAt = (
+            ($this->faker->boolean) ? null : $this->faker->dateTimeBetween($forwardCreatedAt,)
+        );
         return [
             'created_at' => $createdAt ,
             'updated_at' => $updatedAt
         ];
-    }
-    protected function get_created_at(
-        string|\DateTime $start = "-5 years" , string|\DateTime $end = 'now'
-    ):\DateTime{
-        return $this->faker->dateTimeBetween($start,$end);
-    }
-    protected function forwardDateTime(\DateTime $dateTime,int $min=30 , int $max = 3600):\DateTime {
-        $timestamp = $dateTime->getTimestamp();
-        return (clone $dateTime)->setTimestamp($timestamp+rand($min,$max));
-    }
-    protected function get_updated_at(\DateTimeInterface $createdAt):null|\DateTimeInterface {
-        if ($this->faker->boolean)
-            return $this->faker->dateTimeBetween($createdAt,'now');
-        return null;
     }
 }
