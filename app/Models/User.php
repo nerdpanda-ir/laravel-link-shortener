ac1da84980ae32faffddbc839d5e3fd9b7448fb3
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-29 14:19:10 +0200

    [file]{User.php}-> define relation method

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Contracts\UserableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use NerdPanda\Traits\Model\VerifiedEmailScopeTrait;
@@ -43,4 +44,7 @@ class User extends Authenticatable implements UserableContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function roles():BelongsToMany{
+        return $this->belongsToMany(Role::class);
+    }
 }
