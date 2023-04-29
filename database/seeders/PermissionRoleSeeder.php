228a7426b581c18b53c1ae1f82d1b3880e8b020d
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-29 10:14:23 +0200

    [file]{PermissionRoleSeeder.php}=> create seeder

---
+++ database/seeders/PermissionRoleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Contracts\PermissionRoleSeederContract as Contract;
+class PermissionRoleSeeder extends Seeder implements Contract
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
