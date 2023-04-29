82a15a1b95287517a872cafaa1c47ba3aa9ffee4
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-29 09:14:26 +0200

    [files]{exceptions.php}=> modify error message

--- lang/en/exceptions.php
+++ lang/en/exceptions.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'no-dependency-found' => 'no found any :dependency for seeding :seeder '
+    'no-dependency-found' => 'number of :dependency is not enough for seeding :seeder '
 ];
