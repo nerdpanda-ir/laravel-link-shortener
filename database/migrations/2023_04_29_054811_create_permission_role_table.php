f55b2c194017d4e6d8245997f63df39a1fe25329
NerdPanda <nerdpandabusiness@gmail.com>
2023-04-29 07:48:44 +0200

    [file]{2023_04_29_054811_create_permission_role_table.php}-> create migration

---
+++ database/migrations/2023_04_29_054811_create_permission_role_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->timestamps();
+
+            $table->foreign('role_id')
+                    ->on('roles')->references('id')
+                    ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreign('permission_id')
+                ->on('permissions')->references('id')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permission_role');
+    }
+};
