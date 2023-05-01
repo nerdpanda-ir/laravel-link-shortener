97861ec9ede8cbe5465db6e8f5b028260d79ee71
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-01 10:39:29 +0200

    [file]{web.php}=> create route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DoLoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +20,6 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('',HomeController::class)->name('home');
 Route::get('login',LoginController::class)->name('login');
+Route::post('login',DoLoginController::class)->name('login');
 Route::get('register',RegisterController::class)->name('register');
 Route::get('dashboard',DashboardController::class)->name('dashboard');
