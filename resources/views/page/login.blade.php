19ea5746f775a2411758f46bf9b028ac6a9c59d3
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-01 16:55:56 +0200

    [files]{login.blade.php}=> use old values

--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -26,17 +26,18 @@
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
+                <input type="text" class="form-control" id="floatingInput"
+                       placeholder="name@example.com" name="email" value="{{old('email')}}">
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+                <input type="password" class="form-control" id="floatingPassword"
+                       placeholder="Password" name="password"  value="{{old('email')}}">
                 <label for="floatingPassword">Password</label>
             </div>
-
             <div class="checkbox mb-3">
                 <label>
-                    <input type="checkbox" value="remember-me" name="remember"> Remember me
+                    <input type="checkbox" value="remember-me" name="remember" @checked(old('remember'))> Remember me
                 </label>
             </div>
             <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
