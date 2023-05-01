9cf6db3d0c10166c155f6d03f8dac91c9139191c
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-01 10:59:06 +0200

    [file]{DoLoginController.php}=> use request

--- app/Http/Controllers/DoLoginController.php
+++ app/Http/Controllers/DoLoginController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Contracts\DoLoginRequestContract as RequestContract;
 class DoLoginController extends Controller
 {
-    public function __invoke()
+    public function __invoke(RequestContract $request)
     {
-
+        dd(request()->all());
     }
 }
