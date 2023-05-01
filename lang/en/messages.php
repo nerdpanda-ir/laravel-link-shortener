fd27462589af3a01d2b4dd11f7a5b2e256c4111d
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-01 17:09:53 +0200

    [file]{messages.php}-> create some messages

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -7,10 +7,12 @@ return [
             'start' => 'user start authentication' ,
             'fail' => 'user authentication is fail' ,
             'success' => 'user successfully authenticated' ,
+            'exceptionHappen' => 'when user logged in we have exception in application'
         ]
     ],
     'auth'=> [
         'success' => 'welcome :name your successfully logged in ' ,
-        'fail' => 'email or password wrong'
+        'fail' => 'email or password wrong' ,
+        'exceptionHappen' => 'something happened please try after or contact to help and support '
     ]
 ];
