8271c1dd0a21b9707c034f2f8212a6cc8885f9b2
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-02 07:17:19 +0200

    [files]{ home.blade.php , dashboard.blade.php }=> use system-messages-printer component

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <x-partials.nav />
 <main class="container py-5">
+    <x-partials.system-messages-printer />
     <h1>Link Shortener</h1>
     <p>you can short your links with me !!!</p>
     <hr class="my-5">
