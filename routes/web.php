8f0c96888374f73e0e0c2f0afca6e8918884f557
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-02 06:56:59 +0200

    [files]{web.php}=> create logout route

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DoLoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,5 +21,6 @@ use App\Http\Controllers\DashboardController;
 Route::get('',HomeController::class)->name('home');
 Route::get('login',LoginController::class)->name('login')->middleware('guest');
 Route::post('login',DoLoginController::class)->name('do-login')->middleware('guest');
+Route::get('logout',LogoutController::class)->name('logout')->middleware('auth');
 Route::get('register',RegisterController::class)->name('register');
 Route::get('dashboard',DashboardController::class)->name('dashboard');
