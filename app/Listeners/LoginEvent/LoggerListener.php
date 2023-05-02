9fb9038ec02a01a9720b802aff68a1021b287aea
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-02 13:12:43 +0200

    [file]{ LoggerListener.php }=> create listener for log login action

---
+++ app/Listeners/LoginEvent/LoggerListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners\LoginEvent;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Translation\Translator;
+use Psr\Log\LoggerInterface as Logger;
+
+class LoggerListener
+{
+    protected Logger $logger;
+    protected Translator $translator;
+    public function __construct(Logger $logger , Translator $translator)
+    {
+        $this->logger = $logger;
+        $this->translator = $translator;
+    }
+
+    public function handle(Login $event): void
+    {
+        $this->logger->info(
+            $this->translator->get('messages.log.auth.login.ok')
+        );
+    }
+}
