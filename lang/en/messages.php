0795640e0bf7ad0631bfdd079971a933ee17c573
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-02 07:52:32 +0200

    [file]{ messages.php }=> create messages item and modify array strucure

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -4,15 +4,28 @@ return [
         'request' => 'Request Start' ,
         'response' => 'Delivered Response To User' ,
         'auth' => [
-            'start' => 'user start authentication' ,
-            'fail' => 'user authentication is fail' ,
-            'success' => 'user successfully authenticated' ,
-            'exceptionHappen' => 'when user logged in we have exception in application'
+            'login' => [
+                'start' => 'user start login action' ,
+                'fail' => 'user authentication is fail' ,
+                'ok' => 'user successfully authenticated' ,
+                'exceptionThrow' => 'when user logged in we have exception in application' ,
+            ],
+            'logout'=> [
+                'start' => 'user start logout action' ,
+                'success' => 'user successfully logout ' ,
+                'exceptionThrow' => 'when user logout some exception is throw'
+            ]
         ]
     ],
     'auth'=> [
-        'success' => 'welcome :name your successfully logged in ' ,
-        'fail' => 'email or password wrong' ,
-        'exceptionHappen' => 'something happened please try after or contact to help and support '
+        'login' => [
+            'ok' => 'welcome :name your successfully logged in ' ,
+            'fail' => 'email or password wrong' ,
+            'exceptionThrow' => 'something happened please try after or contact to help and support '
+        ],
+        'logout'=> [
+            'ok'=> 'dear :name your successfully logout !!!' ,
+            'exceptionThrow' => 'your logout process is fail please try after or contact to help and support !!!'
+        ]
     ]
 ];
