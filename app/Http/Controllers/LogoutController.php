806b43e29e7570e84e86e551779cc3048b1f8355
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-02 08:21:51 +0200

    [file]{ LogoutController.php }=> complete logout logic

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,22 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Routing\Redirector;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogoutController extends Controller
 {
-    public function __invoke(Auth $auth,Redirector $redirector ): Response
+    public function __invoke(
+        Auth $auth , Redirector $redirector , LoggerInterface $logger , Translator $translator
+    ): Response
     {
-        // log , trans , logout email
+        // log , logout email
         try {
+            $logger->info($translator->get('messages.log.auth.logout.start'));
            $user = $auth->guard('web')->user();
            $auth->guard('web')->logout();
-           return $redirector->route('home')->with('system.messages.ok',["dear {$user->name} your successfully logout !!!"]);
+           return $redirector->route('home')
+                   ->with('system.messages.ok',[
+                       $translator->get('messages.auth.logout.ok', ['name' => $user->name])
+                   ]);
         }catch (\Throwable $exception){
+            $logger->info($translator->get('messages.log.auth.logout.exceptionThrow'));
             report($exception);
-            return $redirector->route('home')->with('system.messages.error',["your logout process is fail please try after or contact to help and support !!!"]);
+            return $redirector->route('home')
+                    ->with('system.messages.error',[
+                        $translator->get('messages.auth.logout.exceptionThrow')
+                    ]);
         }
     }
 }
