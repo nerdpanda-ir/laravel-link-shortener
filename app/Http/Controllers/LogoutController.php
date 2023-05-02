5f51e73e62273001afcddc406522fdbce2f0fa62
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-02 07:08:16 +0200

    [files]{ LogoutController.php }=> create logout logic

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Auth\Factory as Auth;
-use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class LogoutController extends Controller
 {
-    public function __invoke(Auth $auth):Response
+    public function __invoke(Auth $auth,Redirector $redirector ): Response
     {
+        // log , trans , logout email
         try {
-            dump(__METHOD__);
+           $user = $auth->guard('web')->user();
+           $auth->guard('web')->logout();
+           return $redirector->route('home')->with('system.messages.ok',["dear {$user->name} your successfully logout !!!"]);
         }catch (\Throwable $exception){
             report($exception);
+            return $redirector->route('home')->with('system.messages.error',["your logout process is fail please try after or contact to help and support !!!"]);
         }
     }
 }
