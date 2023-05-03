0acf77266cd9b1b25c8278c989fab95044e3678f
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 10:05:00 +0200

    [file]{NotifyUserListener.php}-> change listener logic

--- app/Listeners/LoginEvent/NotifyUserListener.php
+++ app/Listeners/LoginEvent/NotifyUserListener.php
@@ -2,53 +2,20 @@
 
 namespace App\Listeners\LoginEvent;
 
+use App\Jobs\UserLoginNotifyJob;
 use Illuminate\Auth\Events\Login;
-use App\Contracts\LoginMailContract as LoginMail;
-use Illuminate\Contracts\Mail\Factory as MailManager;
 use App\Contracts\DoLoginRequestContract as Request;
-use Illuminate\Contracts\Translation\Translator;
-use Psr\Log\LoggerInterface as Logger;
 
 class NotifyUserListener
 {
-    protected Logger $logger;
-    protected LoginMail $loginMail ;
-    protected MailManager $mailManager;
     protected Request $request;
-    protected Translator $translator;
-
-    public function __construct(
-        Logger $logger, LoginMail $loginMail ,MailManager $mailManager , Request $request , Translator $translator
-    )
+    public function __construct(Request $request)
     {
-        $this->loginMail = $loginMail;
-        $this->mailManager = $mailManager ;
         $this->request = $request;
-        $this->logger  = $logger;
-        $this->translator = $translator ;
     }
 
     public function handle(Login $event): void
     {
-        try {
-            $this->logger->info(
-                $this->translator->get('messages.log.mails.login.start')
-            );
-            $this->loginMail
-                ->with([
-                    'name'=>$event->user->name,'ip'=>$this->request->ip()
-                ])
-                ->to($event->user->email);
-            $this->mailManager->send($this->loginMail);
-            $this->logger->info(
-                $this->translator->get('messages.log.mails.login.done')
-            );
-        }catch (\Throwable $exception){
-            $this->logger->info(
-                $this->translator->get('messages.log.mails.login.exceptionThrow')
-            );
-            report($exception);
-        }
-
+        UserLoginNotifyJob::dispatch($event->user,$this->request->ip());
     }
 }
