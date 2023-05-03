e930ebd94965aa626a5c2037ce33449b1300eca4
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 07:21:53 +0200

    [file]{ login.php }=> create translate for login

---
+++ lang/en/mail/login.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'title' => 'Someone logged into your account' ,
+    'content' => 'hey **:name** someOne with this ip **:ip** logged into your account
+                  if you do this action ، ignore this email else change your password or contact
+                  to help and support'
+];
