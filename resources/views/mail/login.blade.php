9a75f5dbbd38001e7daf2f20a7d869ff98de8c14
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 11:29:20 +0200

    [file]{ login.blade.php }=> pass datetime to translate

--- resources/views/mail/login.blade.php
+++ resources/views/mail/login.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # {{trans('mail/login.title')}}
 
-{{trans('mail/login.content', ['name' => $name, 'ip' => $ip ])}}
+{{trans('mail/login.content', ['name' => $name, 'ip' => $ip , 'dateTime' => $dateTime ])}}
 
 Thanks,<br> {{ config('app.name') }}
 </x-mail::message>
