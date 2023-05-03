dcadadef7dcfb2e4bd64ba305613f63026776ed8
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 07:58:31 +0200

    [file]{ LogProcessingListener.php }=> complete logic

--- app/Listeners/Queue/LogProcessingListener.php
+++ app/Listeners/Queue/LogProcessingListener.php
@@ -3,15 +3,24 @@
 namespace App\Listeners\Queue;
 
 use Illuminate\Queue\Events\JobProcessing;
+use Psr\Log\LoggerInterface as Logger;
+use Illuminate\Contracts\Translation\Translator;
 
 class LogProcessingListener
 {
-    public function __construct()
+    protected Logger $logger;
+    protected Translator $translator;
+    public function __construct(Logger $logger,Translator $translator)
     {
+        $this->logger = $logger;
+        $this->translator = $translator ;
     }
 
     public function handle(JobProcessing $event): void
     {
-
+        $jobName = $event->job->resolveName();
+        $this->logger->info(
+            $this->translator->get('messages.log.jobs.processing', ['name' => $jobName ])
+        );
     }
 }
