d8225c31ee531ccc0a401eb47ea399b4c686a61d
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 10:56:18 +0200

    [file]{ LoginMail.php }=> remove todo

--- app/Mail/LoginMail.php
+++ app/Mail/LoginMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-// @todo can be replace to notification ? yea !!!
+
 class LoginMail extends Mailable implements LoginMailContract
 {
     use Queueable, SerializesModels;
