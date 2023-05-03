94e97e3247cd3551fab4f64fe77a58adda7bb915
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 13:06:34 +0200

    [file]{ UserLogoutLoggerListener.php }=> create listener for Logout event

---
+++ app/Listeners/LogoutEvent/UserLogoutLoggerListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners\LogoutEvent;
+
+use Illuminate\Auth\Events\Logout;
+use Psr\Log\LoggerInterface as Logger;
+use Illuminate\Contracts\Translation\Translator;
+
+class UserLogoutLoggerListener
+{
+    protected Logger $logger;
+    protected Translator $translator;
+    public function __construct(Logger $logger, Translator $translator)
+    {
+        $this->logger = $logger;
+        $this->translator = $translator;
+    }
+
+    public function handle(Logout $event): void
+    {
+       $this->logger->info(
+           $this->translator->get('logout')
+       );
+    }
+}
