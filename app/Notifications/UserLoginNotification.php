685eb1d8e8a2d561ef858312ea00ca70a6486b00
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 09:49:53 +0200

    [file]{ UserLoginNotification.php }=> create notification for notify user when login into system

---
+++ app/Notifications/UserLoginNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Contracts\UserableContract;
+use Illuminate\Contracts\Mail\Mailable;
+use Illuminate\Notifications\Notification;
+use App\Contracts\UserLoginNotificationContract as Contract;
+use App\Contracts\LoginMailContract;
+
+class UserLoginNotification extends Notification implements Contract
+{
+    protected Mailable $mail;
+    protected string $ip;
+    public function __construct(LoginMailContract $mail , string $ip)
+    {
+        $this->mail = $mail;
+        $this->ip = $ip;
+    }
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(UserableContract $notifiable): Mailable
+    {
+        return $this->mail
+                    ->to($notifiable->email)
+                    ->with([
+                        'name'=> $notifiable->name,
+                        'ip' => $this->ip
+                    ]);
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [];
+    }
+}
