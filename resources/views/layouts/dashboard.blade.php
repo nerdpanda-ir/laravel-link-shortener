2487722e8bad72e2d9302dbed0519b52d63a8031
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 17:14:18 +0200

    [file]{ dashboard.blade.php }=> use nav component for dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -10,7 +10,7 @@
     <x-partials.dashboard.header />
     <div class="container-fluid">
         <div class="row">
-
+            <x-partials.dashboard.nav />
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <x-partials.system-messages-printer />
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
