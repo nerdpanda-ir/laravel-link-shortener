cf51060806e0ae4e9e31b712ff1026b5ae30eccb
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-03 08:57:02 +0200

    [file]{ LogProcessingListener.php }=> extends from base class and remove logger , translator property from class and in usage replace with getter method

--- app/Listeners/Job/LogProcessingListener.php
+++ app/Listeners/Job/LogProcessingListener.php
@@ -3,24 +3,16 @@
 namespace App\Listeners\Job;
 
 use Illuminate\Queue\Events\JobProcessing;
-use Psr\Log\LoggerInterface as Logger;
-use Illuminate\Contracts\Translation\Translator;
+use App\Services\JobLoggerListener;
 
-class LogProcessingListener
+
+class LogProcessingListener extends JobLoggerListener
 {
-    protected Logger $logger;
-    protected Translator $translator;
-    public function __construct(Logger $logger,Translator $translator)
-    {
-        $this->logger = $logger;
-        $this->translator = $translator ;
-    }
-
     public function handle(JobProcessing $event): void
     {
         $jobName = $event->job->resolveName();
-        $this->logger->info(
-            $this->translator->get('messages.log.jobs.processing', ['name' => $jobName ])
+        $this->getLogger()->info(
+            $this->getTranslator()->get('messages.log.jobs.processing', ['name' => $jobName ])
         );
     }
 }
