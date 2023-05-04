0bafcb7455cf61a2667b121186bc645908b21c19
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-04 07:00:12 +0200

    [file]{ gate.php }=> remove gate config

--- config/gate.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Services\Gates\SystemMonitor;
-?>
-<?php
-return [
-    'see-ram-usage' => [SystemMonitor::class,'ramUsage'] ,
-    'see-disk-usage' => [SystemMonitor::class,'diskUsage']
-];
