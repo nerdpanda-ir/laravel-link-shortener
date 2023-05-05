cba903f39f7bbb9864b91ef6907baca00a415f4b
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-05 13:55:22 +0200

    [file]{ LoadUserPermissionsListener.php }=> create listener

---
+++ app/Listeners/AuthenticatedEvent/LoadUserPermissionsListener.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Listeners\AuthenticatedEvent;
+
+use Illuminate\Auth\Events\Authenticated;
+
+class LoadUserPermissionsListener
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(Authenticated $event): void
+    {
+        $event->user->load('permissions');
+    }
+}
