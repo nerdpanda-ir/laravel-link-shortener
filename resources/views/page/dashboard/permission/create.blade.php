272bbafb082ded2d8deeeda60368168a55367113
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 14:12:25 +0200

    [file]{ create.blade.php }=> make create permission form

--- resources/views/page/dashboard/permission/create.blade.php
+++ resources/views/page/dashboard/permission/create.blade.php
@@ -3,5 +3,27 @@
     <h1>create new permission</h1>
 @endsection
 @section('dashboard-content')
-    <p>create a new permission is very cool</p>
+    <x-alert class="bg-dark text-warning">
+        <x-slot:message>
+            name should be unique when you create a new permission care that !!!
+        </x-slot:message>
+    </x-alert>
+    <hr class="my-4">
+    <form class="needs-validation" novalidate="">
+        <div class="row g-3">
+            <div class="col-12">
+                <label for="name" class="form-label">Name</label>
+                <div class="input-group has-validation">
+                    <span class="input-group-text">@</span>
+                    <input type="text" class="form-control" id="name" placeholder="modify-something" name="name">
+                    <div class="invalid-feedback">
+                        Your username is required.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="my-4">
+        <button class="w-100 btn btn-primary btn-lg" type="submit">Save</button>
+    </form>
+
 @endsection
