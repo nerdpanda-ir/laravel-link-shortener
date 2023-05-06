4537b311b52aba207e9ca18b3b85020095ab0423
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 13:29:39 +0200

    [file]{ create.blade.php }=> make create view for create new permission

---
+++ resources/views/page/dashboard/permission/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+@section('dashboard-title')
+    <h1>create new permission</h1>
+@endsection
+@section('dashboard-content')
+    <p>create a new permission is very cool</p>
+@endsection
