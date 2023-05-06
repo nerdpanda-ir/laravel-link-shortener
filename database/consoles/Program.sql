a9df278932ecd2507556238f92000739bf8a2388
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 14:32:31 +0200

    [file]{ Program.sql }=> modify

--- database/consoles/Program.sql
+++ database/consoles/Program.sql
@@ -1,1 +1,12 @@
-#SET GLOBAL FOREIGN_KEY_CHECKS = 0;
+delete from `permissions`;
+delete from `permission_user`;
+insert into permissions values
+(null , 'permission-view-all'  , 1 , now() , null ) ,
+(null , 'permission-create'  , 1 , now() , null ) ;
+insert into `permission_user` values
+(
+ null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-view-all' ) , 1 , now() , null
+),
+(
+ null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-create' ) , 1 , now() , null
+);
