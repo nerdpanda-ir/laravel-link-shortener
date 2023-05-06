4e30ed25fd1a58453bff969c2a35284ea9da372d
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 13:51:18 +0200

    [file]{ dashboard.php }=> create permission group and make create permission route

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,3 +2,8 @@
 
 Route::get('/','HomeController')->name('home');
 
+Route::name('permission.')->prefix('permission')
+        ->group(function (){
+            Route::get('create','Permission\CreateController')
+                    ->name('create')->middleware('can:create-permission');
+        });
