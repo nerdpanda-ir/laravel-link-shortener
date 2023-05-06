3e291108f5a5a499a07f14a3104df2347baa2be6
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 15:20:50 +0200

    [file]{ StoreRequest.php }=> create request for store permission

---
+++ app/Http/Requests/Dashboard/Permission/StoreRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Dashboard\Permission;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Contracts\Requests\Dashboard\Permission\StoreRequest as Contract;
+class StoreRequest extends FormRequest implements Contract
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
