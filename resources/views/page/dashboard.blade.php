a6e751c9ffb56a6fe44801d1fadf30f1e917fda8
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 09:39:17 +0200

    [file]{ dashboard.blade.php }=> attach dashboard stylesheet

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -1,34 +1,7 @@
 @extends('layouts.dashboard')
 @section('css')
     @parent
-    <style>
-    .feature-icon {
-    width: 5rem;
-    height: 5rem;
-    border-radius: .75rem;
-    }
-
-    .icon-square {
-    width: 3rem;
-    height: 3rem;
-    border-radius: .75rem;
-    }
-
-    .text-shadow-1 { text-shadow: 0 .125rem .25rem rgba(0, 0, 0, .25); }
-    .text-shadow-2 { text-shadow: 0 .25rem .5rem rgba(0, 0, 0, .25); }
-    .text-shadow-3 { text-shadow: 0 .5rem 1.5rem rgba(0, 0, 0, .25); }
-
-    .card-cover {
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    }
-
-    .feature-icon-small {
-    width: 3rem;
-    height: 3rem;
-    }
-    </style>
+    <link rel="stylesheet" href="{{asset('css/page/dashboard.css')}}">
 @endsection
 @section('dashboard-content')
     <section class="row justify-content-evenly">
