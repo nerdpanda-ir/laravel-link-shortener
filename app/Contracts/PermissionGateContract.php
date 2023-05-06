7fbd00a79f4a80367b90617eb4816f72055777bf
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-06 13:07:53 +0200

    [file]{ PermissionGateContract.php }=> extend from GateCreateableContract and create alias for contracts

--- app/Contracts/PermissionGateContract.php
+++ app/Contracts/PermissionGateContract.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Contracts;
-
-interface PermissionGateContract extends GateViewAllableContract
+use App\Contracts\GateViewAllableContract as Viewallable;
+use App\Contracts\GateCreateableContract as Createable;
+interface PermissionGateContract extends Viewallable , Createable
 {
 
 }
