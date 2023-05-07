7476881a605305e6fb36b17614ca5a19c8301d47
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 10:03:53 +0200

    [files]{ StoreBuilder.php } -> extends translatorGetterable for StoreBuilder.php contract

--- app/Contracts/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Contracts/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Contracts\Responses\Dashboard\Permission\Store;
+use App\Contracts\TranslatorGetterable;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-interface StoreBuilder
+interface StoreBuilder extends TranslatorGetterable
 {
     public function build(string $permission): RedirectResponse;
 }
