617973b4ba81bb3adf914a9e4ee0c93bee2e1bf0
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 10:08:43 +0200

    [files]{ StoreBuilder.php } -> define translator property

--- app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -4,11 +4,16 @@ namespace App\Http\Responses\Dashboard\Permission\Store;
 
 use App\Services\ResponseBuilder;
 use App\Traits\TranslatorGetterable;
+use Illuminate\Contracts\Routing\ResponseFactory as Response;
+use Illuminate\Contracts\Translation\Translator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use App\Contracts\Responses\Dashboard\Permission\Store\StoreBuilder as Contract;
 class StoreBuilder extends ResponseBuilder implements Contract
 {
     use TranslatorGetterable;
+    protected Translator $translator;
+
+
     public function build(string $permission): RedirectResponse
     {
         return $this->response->redirectToRoute('dashboard.home')
