88a33997b8497b6120d5db63c2736bbf3b49e65d
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 15:15:24 +0200

    [file]{ Permission.php }-> remove view all method in permission gate service

--- app/Services/Gates/Permission.php
+++ app/Services/Gates/Permission.php
@@ -9,11 +9,7 @@ use App\Traits\Services\Gates\Permission\Editable;
 class Permission extends GateBase implements Contract
 {
     use Deleteable , Editable ;
-    public function viewAll(): bool
-    {
-        return $this->getPermissionManager()
-                    ->has('permission-view-all');
-    }
+
 
     public function create(): bool
     {
