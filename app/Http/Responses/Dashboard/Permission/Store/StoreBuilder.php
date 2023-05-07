c31baef0da4aaafa796476a4eb02d37234876b9f
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 09:33:40 +0200

    [files]{ StoreBuilder.php } -> modify service namespace and modify contract namespace

--- app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Responses\Permission\Store;
+namespace App\Http\Responses\Dashboard\Permission\Store;
 
 use App\Services\ResponseBuilder;
 use Symfony\Component\HttpFoundation\Response;
-use \App\Contracts\Responses\Permission\Store\StoreBuilder as Contract;
+use App\Contracts\Responses\Dashboard\Permission\Store\StoreBuilder as Contract;
 class StoreBuilder extends ResponseBuilder implements Contract
 {
     public function build(string $permission , string $okMessage): Response
