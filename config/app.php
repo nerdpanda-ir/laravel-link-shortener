3817bc1d503e2f9c814a13d25d60a37e134b07ae
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 14:44:09 +0200

    [file]{  app.php }-> register facade to application

--- config/app.php
+++ config/app.php
@@ -209,6 +209,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+         'DateServiceFacade' => \App\Facades\DateServiceFacade::class,
     ])->toArray(),
 
 ];
