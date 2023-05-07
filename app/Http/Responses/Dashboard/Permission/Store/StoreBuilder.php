a161193d7d682ddb10f67a6766cd23ccebf29c7e
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 10:02:15 +0200

    [files]{ StoreBuilder.php } -> remove okMessage parameter from service

--- app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -3,11 +3,12 @@
 namespace App\Http\Responses\Dashboard\Permission\Store;
 
 use App\Services\ResponseBuilder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use App\Contracts\Responses\Dashboard\Permission\Store\StoreBuilder as Contract;
 class StoreBuilder extends ResponseBuilder implements Contract
 {
-    public function build(string $permission , string $okMessage): Response
+    public function build(string $permission): Response
     {
         return $this->response->redirectToRoute('dashboard.home')
                     ->with('system.messages.ok', [$okMessage]);
