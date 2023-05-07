958cc94e4f2a20f1565dcad79ed13bfcafe1b999
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 11:20:18 +0200

    [file]{  dashboard.php }-> create view all permissions route !!!

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,8 +4,12 @@ Route::get('/','HomeController')->name('home');
 
 Route::name('permission.')->prefix('permission')->namespace('Permission')
         ->group(function (){
+            Route::get('all','ViewAllController')
+                ->name('view-all')->middleware('can:view-all-permissions');
+
             Route::get('create','CreateController')
                     ->name('create')->middleware('can:create-permission');
+
             Route::post('store','StoreController')
                     ->name('store')->middleware('can:create-permission');
         });
