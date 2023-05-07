939e930243283db64edab5844cfce367ca14c41b
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 12:51:00 +0200

    [file]{  Permission.php }-> define user relation for permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,8 +6,13 @@ use App\Contracts\PermissionModelContract as Contract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Model\DisableTimestamp;
+use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Permission extends Model implements Contract
 {
     use HasFactory , DisableTimestamp;
+    public function creator():BelongsTo{
+        return $this->belongsTo(User::class,'created_by');
+    }
 }
