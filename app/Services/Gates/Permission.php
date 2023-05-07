97099dc48876b3d98fe13570994649397e586324
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 15:14:56 +0200

    [file]{ Permission.php }-> use from deleteable and editable trait

--- app/Services/Gates/Permission.php
+++ app/Services/Gates/Permission.php
@@ -3,9 +3,12 @@
 namespace App\Services\Gates;
 use App\Services\GateBase;
 use App\Contracts\PermissionGateContract as Contract;
+use App\Traits\Services\Gates\Permission\Deleteable;
+use App\Traits\Services\Gates\Permission\Editable;
 
 class Permission extends GateBase implements Contract
 {
+    use Deleteable , Editable ;
     public function viewAll(): bool
     {
         return $this->getPermissionManager()
