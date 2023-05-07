8c29157123bf4cdc712f6157376fe7e3e547e8df
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 10:07:49 +0200

    [files]{ StoreBuilder.php } -> use translatorGetterable trait

--- app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -3,10 +3,12 @@
 namespace App\Http\Responses\Dashboard\Permission\Store;
 
 use App\Services\ResponseBuilder;
+use App\Traits\TranslatorGetterable;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use App\Contracts\Responses\Dashboard\Permission\Store\StoreBuilder as Contract;
 class StoreBuilder extends ResponseBuilder implements Contract
 {
+    use TranslatorGetterable;
     public function build(string $permission): RedirectResponse
     {
         return $this->response->redirectToRoute('dashboard.home')
