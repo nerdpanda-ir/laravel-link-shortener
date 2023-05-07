1f9efe6894ec219b93e52cf78701090bbba784ad
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 09:52:02 +0200

    [files]{ StoreBuilder.php } -> remove okMessage parameter

--- app/Contracts/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Contracts/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Contracts\Responses\Dashboard\Permission\Store;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
 interface StoreBuilder
 {
-
+    public function build(string $permission): RedirectResponse;
 }
