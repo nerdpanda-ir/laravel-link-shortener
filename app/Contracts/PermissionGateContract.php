2596f8958840db958c458811a4e57cf0a2ad29db
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 15:03:11 +0200

    [file]{ PermissionGateContract.php }-> extend from editable and deleteable

--- app/Contracts/PermissionGateContract.php
+++ app/Contracts/PermissionGateContract.php
@@ -3,7 +3,9 @@
 namespace App\Contracts;
 use App\Contracts\GateViewAllableContract as Viewallable;
 use App\Contracts\GateCreateableContract as Createable;
-interface PermissionGateContract extends Viewallable , Createable
+use App\Contracts\GateEditable as Editable;
+use App\Contracts\GateDeleteable as Deleteable ;
+interface PermissionGateContract extends Viewallable , Createable , Editable , Deleteable
 {
 
 }
