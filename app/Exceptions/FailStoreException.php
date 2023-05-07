c875a1342a52525703fae42822cd67de752ac642
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 08:11:58 +0200

    [file]{FailStoreException.php } -> create setMessage Method

--- app/Exceptions/FailStoreException.php
+++ app/Exceptions/FailStoreException.php
@@ -6,4 +6,9 @@ use Exception;
 use App\Contracts\Exceptions\FailStore as Contract;
 class FailStoreException extends Exception implements Contract
 {
+    public function setMessage(string $message): void
+    {
+        $this->message = $message ;
+    }
+
 }
