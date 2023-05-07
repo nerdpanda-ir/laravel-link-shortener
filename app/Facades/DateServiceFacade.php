e83fcc03af80a50c9a95ec0b4fb2466120b66d57
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 14:46:21 +0200

    [file]{  DateServiceFacade.php }-> create dock block for suggest method in ide

--- app/Facades/DateServiceFacade.php
+++ app/Facades/DateServiceFacade.php
@@ -3,8 +3,10 @@
 namespace App\Facades;
 
 use App\Contracts\Services\DateService;
+use Carbon\CarbonInterface;
 use Illuminate\Support\Facades\Facade;
 
+/** @method CarbonInterface|null dateStrToCarbon(string $dateStr) */
 class DateServiceFacade extends Facade
 {
     protected static function getFacadeAccessor():string
