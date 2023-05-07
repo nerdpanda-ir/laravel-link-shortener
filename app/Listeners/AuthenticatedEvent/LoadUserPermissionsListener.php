1f20348f60084dbb0d943488ede763aa41733a15
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 17:17:06 +0200

    [file]{ LoadUserPermissionsListener.php } -> create todo comment

--- app/Listeners/AuthenticatedEvent/LoadUserPermissionsListener.php
+++ app/Listeners/AuthenticatedEvent/LoadUserPermissionsListener.php
@@ -12,6 +12,10 @@ class LoadUserPermissionsListener
 
     public function handle(Authenticated $event): void
     {
+        /**
+         * @TODO THIS IS LAYZY APPROACH THAT IS UNOPTIMIZE SHOULD CHECK USERPERMISSION WITH EXIST SQL AND
+         *  FLY WEIGHT AND SHOULD HAVE TWO STRATEGY 1- LAYZY STRATEGY 2- SQL EXIST AND FLY WEIGHT STRATEGY
+         */
         $event->user->load('permissions:permissions.name');
     }
 }
