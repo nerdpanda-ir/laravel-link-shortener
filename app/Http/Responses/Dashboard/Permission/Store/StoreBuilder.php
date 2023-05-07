896e856e2dacd8c274573323fa6dce69a460a950
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 10:03:06 +0200

    [files]{ StoreBuilder.php } -> modify return type for build method to RedirectResponse

--- app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
+++ app/Http/Responses/Dashboard/Permission/Store/StoreBuilder.php
@@ -4,11 +4,10 @@ namespace App\Http\Responses\Dashboard\Permission\Store;
 
 use App\Services\ResponseBuilder;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use App\Contracts\Responses\Dashboard\Permission\Store\StoreBuilder as Contract;
 class StoreBuilder extends ResponseBuilder implements Contract
 {
-    public function build(string $permission): Response
+    public function build(string $permission): RedirectResponse
     {
         return $this->response->redirectToRoute('dashboard.home')
                     ->with('system.messages.ok', [$okMessage]);
