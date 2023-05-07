103416a31efff5da63a3d07341b6676c32a485dd
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 15:18:05 +0200

    [file]{ Permission.php } -> use ViewAllable , Createable  trait

--- app/Services/Gates/Permission.php
+++ app/Services/Gates/Permission.php
@@ -3,13 +3,12 @@
 namespace App\Services\Gates;
 use App\Services\GateBase;
 use App\Contracts\PermissionGateContract as Contract;
+use App\Traits\Services\Gates\Permission\Createable;
 use App\Traits\Services\Gates\Permission\Deleteable;
 use App\Traits\Services\Gates\Permission\Editable;
+use App\Traits\Services\Gates\Permission\ViewAllable;
 
 class Permission extends GateBase implements Contract
 {
-    use Deleteable , Editable ;
-
-
-
+    use ViewAllable , Createable , Deleteable , Editable ;
 }
