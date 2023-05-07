fca43ecf2b28999ce6980852a76e789b4f66525f
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 17:21:31 +0200

    [file]{ edit.blade.php } -> create edit view for display permission edit form

---
+++ resources/views/page/dashboard/permission/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+@section('dashboard-title')
+    <h1> Edit Permission `` {{$name}} ``</h1>
+@endsection
+@section('dashboard-content')
+    <h6> here we should display edit form </h6>
+@endsection
