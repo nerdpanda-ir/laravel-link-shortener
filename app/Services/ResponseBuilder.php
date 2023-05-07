69e0b17b5023e57cde9033209841974f5ff03f8e
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-07 08:42:59 +0200

    [file]{ResponseBuilder.php} -> create service

---
+++ app/Services/ResponseBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\Services\ResponseBuilder\Contract;
+use Illuminate\Contracts\Routing\ResponseFactory as Response;
+
+abstract class ResponseBuilder implements Contract
+{
+    protected Response $response;
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return Response
+     */
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
+
+}
