676f8c39e27fa309bc6bcc7e225aaa1c0c7889c9
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-08 10:24:40 +0200

    [file]{ update.php } -> create request for update permission

---
+++ app/Http/Requests/Dashboard/Permission/Update.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Dashboard\Permission;
+use App\Contracts\Requests\Dashboard\Permission\Update as Contract;
+use App\Contracts\Rule\UniqueExcept;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Auth\Factory as Auth;
+class Update extends FormRequest implements Contract
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(Auth $auth): bool
+    {
+        return $auth
+                    ->guard('web')
+                    ->user()
+                    ->can('edit-permission');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(UniqueExcept $uniqueExcept): array
+    {
+        $uniqueExcept->setExcepts([$this->route()->parameter('name')]);
+        $uniqueExcept->setTableName('permissions');
+        return [
+            'name'=> ['required','max:64',$uniqueExcept]
+        ];
+    }
+}
