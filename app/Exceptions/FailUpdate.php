fbfb1a4dd1b2715c011194c8dcd2786e21c88664
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-08 11:24:49 +0200

    [file]{ FailUpdate.php } -> use set messageable trait !!!

--- app/Exceptions/FailUpdate.php
+++ app/Exceptions/FailUpdate.php
@@ -4,7 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Contracts\Exceptions\FailUpdate as Contract;
+use App\Traits\SetMessageable;
 class FailUpdate extends Exception implements Contract
 {
-    //
+    use SetMessageable;
 }
