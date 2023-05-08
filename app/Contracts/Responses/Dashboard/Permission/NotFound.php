8ba90f899ee359529f0735c61ea8ad95b0178b16
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-08 05:55:20 +0200

    [file]{ NotFoundPermission.php } -> rename to NotFound.php

--- app/Contracts/Responses/Dashboard/Permission/NotFound.php
+++ app/Contracts/Responses/Dashboard/Permission/NotFound.php
@@ -5,7 +5,7 @@ namespace App\Contracts\Responses\Dashboard\Permission;
 use App\Contracts\TranslatorGetterable;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-interface NotFoundPermission extends TranslatorGetterable
+interface NotFound extends TranslatorGetterable
 {
     public function build(string $permission):RedirectResponse;
 }
