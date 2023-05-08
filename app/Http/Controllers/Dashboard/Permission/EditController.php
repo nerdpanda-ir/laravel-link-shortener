6c99eb2467ebc2985e8770d06641c6cacf9bc469
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-08 06:35:02 +0200

    [file]{ EditController.php } -> complete edit logic

--- app/Http/Controllers/Dashboard/Permission/EditController.php
+++ app/Http/Controllers/Dashboard/Permission/EditController.php
@@ -2,32 +2,49 @@
 
 namespace App\Http\Controllers\Dashboard\Permission;
 
+use App\Contracts\Responses\Dashboard\Permission\Edit\ExceptionThrow;
 use App\Http\Controllers\Controller;
 use App\Contracts\PermissionModelContract as Permission;
-use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\View\Factory as ViewFactory;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Contracts\Responses\Dashboard\Permission\NotFound;
+use Psr\Log\LoggerInterface as Logger;
+use Illuminate\Contracts\Translation\Translator ;
 
 class EditController extends Controller
 {
 
     /**
-     * @param string $name
      * @param \App\Models\Permission $model
-     * @param ViewFactory $viewFactory
-     * @return View|RedirectResponse
+     * @return void
      */
     public function __invoke(
-        string $name , Permission $model , ViewFactory $viewFactory ,
-    ):View|RedirectResponse
+        string $name , Permission $model , ViewFactory $viewFactory ,  Logger $logger , Translator $translator ,
+        NotFoundHttpException $notFoundHttpException , ExceptionHandler $exceptionHandler ,
+        NotFound $notFoundResponseBuilder , ExceptionThrow $exceptionThrowResponseBuilder ,
+    ): View | RedirectResponse
     {
         try {
-            $permission = $model->where('name','=',$name)->get(['name']);
+            $permission = $model->where('name','=',$name)->first(['name']);
             if (is_null($permission))
-                abort(404);
-            dump($permission);
-        }catch (\Throwable $exception){
-            dump($exception->getMessage());
+                throw $notFoundHttpException;
+            return $viewFactory->make('page.dashboard.permission.edit',compact('name'));
+        }catch (NotFoundHttpException $exception){
+            return $notFoundResponseBuilder->build($name);
+            // if should you render 404 page you must render exception with laravel exception handler !!!
+            // you can get exception handler from container and call render method or
+            #return $exceptionHandler->render(\request(),$exception);
+            // throw again exception
+            //throw $exception;
+        } catch (\Throwable $exception){
+            $logger->emergency(
+                $translator->get('messages.log.edit.permission.exceptionThrow', ['permission' => $name ])
+            );
+            $exceptionHandler->report($exception);
+            return $exceptionThrowResponseBuilder->build($name);
         }
     }
 }
