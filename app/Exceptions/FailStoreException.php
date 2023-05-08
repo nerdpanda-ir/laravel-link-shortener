ee0fb549845c57aae6851e02dac73dc92562782b
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-08 11:25:18 +0200

    [file]{ FailStoreException.php } -> use set messageable trait !!!

--- app/Exceptions/FailStoreException.php
+++ app/Exceptions/FailStoreException.php
@@ -2,13 +2,10 @@
 
 namespace App\Exceptions;
 
+use App\Traits\SetMessageable;
 use Exception;
 use App\Contracts\Exceptions\FailStore as Contract;
 class FailStoreException extends Exception implements Contract
 {
-    public function setMessage(string $message): void
-    {
-        $this->message = $message ;
-    }
-
+    use SetMessageable;
 }
