da895c82ae331de812e0aba2b508a06720011bbf
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-08 13:16:55 +0200

    [file]{ Fail.php } -> create fail update permission response builder

---
+++ app/Http/Responses/Dashboard/Permission/Update/Fail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Responses\Dashboard\Permission\Update;
+use App\Services\ResponseBuilderContainTranslator as ResponseBuilder;
+use App\Contracts\Responses\Dashboard\Permission\Update\Fail as Contract;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class Fail extends ResponseBuilder implements Contract
+{
+    public function build(string $permission, array $inputs): RedirectResponse
+    {
+        return $this->getResponse()
+                    ->redirectToRoute('dashboard.permission.edit',$permission)
+                    ->withInput($inputs)
+                    ->with('system.messages.error',[
+                        $this->getTranslator()->get(
+                            'messages.update.permission.fail', ['permission' => $permission]
+                        )
+                    ]);
+    }
+}
