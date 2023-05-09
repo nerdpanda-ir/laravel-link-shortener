c327cb04662ee60943a06e4cd1cb21ded0ef948c
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 20:48:33 +0200

    [file] { PermissionFactoryServiceProvider.php }-> move to factories directory and rename to PermissionServiceProvider.php

--- app/Providers/Factories/PermissionServiceProvider.php
+++ app/Providers/Factories/PermissionServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Providers;
+namespace App\Providers\Factories;
 
 use App\Contracts\Factories\Permission;
 use Database\Factories\PermissionFactory;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class PermissionFactoryServiceProvider extends ServiceProvider implements DeferrableProvider
+class PermissionServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public function register(): void
     {
