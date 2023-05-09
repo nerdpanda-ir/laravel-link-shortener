b3bdc947ed6120e35c9c3c0a1504109e7d44326f
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 16:34:07 +0200

    [file]{ Fail.php } => create Response Builder !!!

---
+++ app/Http/Responses/Dashboard/Permission/Delete/Fail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Responses\Dashboard\Permission\Delete;
+use App\Contracts\Responses\Dashboard\Permission\Delete\Fail as Contract ;
+use App\Services\ResponseBuilderContainTranslator as ResponseBuilder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class Fail extends ResponseBuilder implements Contract
+{
+    public function build(string $permission): RedirectResponse
+    {
+        return $this
+                    ->getResponse()
+                    ->redirectToRoute('dashboard.permission.view-all')
+                    ->with('system.messages.errors',[
+                        $this->getTranslator()->get(
+                            'messages.delete.permission.fail', ['name' => $permission]
+                        )
+                    ]);
+    }
+}
