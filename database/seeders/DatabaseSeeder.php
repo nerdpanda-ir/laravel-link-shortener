dad01892d8930895e812d2f5a1b6f089d76ff5f5
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 20:45:44 +0200

    [file] { DatabaseSeeder.php }-> remove useless use commands

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Contracts\PermissionFactoryContract;
-use Database\Factories\RoleFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
