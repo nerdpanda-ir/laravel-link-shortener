aaa246fed020a2634aab15e26ea69218565cc4b0
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 15:08:08 +0200

    [file]{ ThrowException.php } => create response builder

---
+++ app/Http/Responses/Dashboard/Permission/Delete/ThrowException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Responses\Dashboard\Permission\Delete;
+
+use App\Services\ResponseBuilderContainTranslator as ResponseBuilder;
+use App\Contracts\Responses\Dashboard\Permission\Delete\ThrowException as Contract;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class ThrowException extends ResponseBuilder implements Contract
+{
+    public function build(string $permission): RedirectResponse
+    {
+        return $this
+                    ->getResponse()
+                    ->redirectToRoute('dashboard.permission.view-all')
+                    ->with(
+                        'system.messages.error' , [
+                            $this->getTranslator()->get(
+                                'messages.delete.permission.throwException',
+                                ['name' => $permission, 'date' => date("Y-m-d H:i:s")]
+                            )
+                        ]
+                    );
+    }
+}
