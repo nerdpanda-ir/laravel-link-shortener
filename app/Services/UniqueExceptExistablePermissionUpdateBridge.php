3d50a945768edb8b2e9683f3090977139b974ede
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 13:13:09 +0200

    [file]{ UniqueExceptExistablePermissionUpdateBridge.php }-> create service

---
+++ app/Services/UniqueExceptExistablePermissionUpdateBridge.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\Services\UniqueExceptExistablePermissionUpdateBridge as Contract;
+class UniqueExceptExistablePermissionUpdateBridge
+    extends UniqueExceptExistableBridge
+    implements Contract
+{
+    protected array $excepts ;
+    protected string $only;
+    public function build(): bool
+    {
+        return $this->getDatabaseManager()
+                    ->table('permissions')
+                    ->whereNotIn('id',$this->getExcepts())
+                    ->where('name','=',$this->getOnly())
+                    ->exists();
+    }
+
+    /**
+     * @return array
+     */
+    public function getExcepts(): array
+    {
+        return $this->excepts;
+    }
+
+    /**
+     * @param array $excepts
+     */
+    public function setExcepts(array $excepts): void
+    {
+        $this->excepts = $excepts;
+    }
+
+    public function getOnly(): string
+    {
+        return $this->only;
+    }
+
+    public function setOnly(string $only):void
+    {
+        $this->only = $only;
+    }
+
+
+}
