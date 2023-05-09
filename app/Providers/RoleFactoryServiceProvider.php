cd8fcfda87e45120d5a1482ad368cafd887ce9c8
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 20:55:48 +0200

    [files] { RoleFactory.php , RoleSeeder.php , RoleFactoryServiceProvider.php }=> modify name and namespace for contract

--- app/Providers/RoleFactoryServiceProvider.php
+++ app/Providers/RoleFactoryServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Contracts\RoleFactoryContract;
+use App\Contracts\Factories\Role;
 use Database\Factories\RoleFactory;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
@@ -11,13 +11,13 @@ class RoleFactoryServiceProvider extends ServiceProvider implements DeferrablePr
 {
     public function register(): void
     {
-        $this->app->bind(RoleFactoryContract::class, RoleFactory::class);
-        $this->app->alias(RoleFactoryContract::class,'contract.factory.role');
+        $this->app->bind(Role::class, RoleFactory::class);
+        $this->app->alias(Role::class,'contract.factory.role');
     }
     public function provides():array
     {
         return [
-            RoleFactoryContract::class,'contract.factory.role'
+            Role::class,'contract.factory.role'
         ];
     }
 }
