2d6a576c60382a9af8bf64ea8f50b6d45aff96c8
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 11:56:55 +0200

    [file]{ UniqueExceptQueryBuilder.php }-> create contract

---
+++ app/Contracts/Services/UniqueExceptExistableBridge.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Contracts\Services;
+use App\Contracts\DatabaseManagerGetterable;
+use Illuminate\Contracts\Database\Query\Builder;
+
+interface UniqueExceptExistableBridge extends DatabaseManagerGetterable
+{
+    public function build():Builder;
+}
