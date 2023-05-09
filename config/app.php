383905ed6fef3ec93fa34ad519ff830950f9587d
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 15:12:22 +0200

    [file]{ app.php } => register ThrowExceptionServiceProvider to app

--- config/app.php
+++ config/app.php
@@ -203,6 +203,8 @@ return [
         \App\Providers\Responses\Dashboard\Permission\Update\FailServiceProvider::class ,
         \App\Providers\Responses\Dashboard\Permission\Update\OkServiceProvider::class ,
         \App\Providers\Services\UniqueExceptPermissionUpdateBridge::class ,
+        \App\Providers\Responses\Dashboard\Permission\Delete\ThrowExceptionServiceProvider::class ,
+
     ])->toArray(),
 
     /*
