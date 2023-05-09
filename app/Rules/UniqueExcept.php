b3d70e9b00df1b11b8d19941a8cefc0ccfb05085
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-09 13:14:13 +0200

    [file]{ UniqueExcept.php }-> remove some methods and make many methods in service !!!

--- app/Rules/UniqueExcept.php
+++ app/Rules/UniqueExcept.php
@@ -10,20 +10,14 @@ use Illuminate\Contracts\Validation\ValidationRule;
 use App\Contracts\Rule\UniqueExcept as Contract;
 use Illuminate\Database\ConnectionResolverInterface;
 use Illuminate\Database\ConnectionResolverInterface as DatabaseManager;
+use App\Contracts\Services\UniqueExceptExistableBridge as ExistableBridge;
 class UniqueExcept implements ValidationRule , Contract
 {
-    use DatabaseManagerGetterable , TranslatorGetterable;
-    protected array $excepts = [];
-    protected string $columnName;
-    protected string $tableName;
-    /**
-     * @var \Illuminate\Database\DatabaseManager $databaseManager
-     */
-    protected DatabaseManager $databaseManager;
+    use TranslatorGetterable;
     protected Translator $translator;
-    public function __construct(DatabaseManager $databaseManager , Translator $translator)
+    protected ExistableBridge $existableBridge;
+    public function __construct(Translator $translator)
     {
-        $this->databaseManager = $databaseManager;
         $this->translator = $translator ;
     }
 
@@ -35,50 +29,29 @@ class UniqueExcept implements ValidationRule , Contract
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $column = $this->getColumnName()??$attribute;
-        $isExists  = $this->databaseManager
-                            ->table($this->getTableName())
-                            ->whereNotIn($column,$this->getExcepts())
-                            ->where($column,'=',$value)
-                            ->exists();
+        $isExists  = $this->getExistableBridge()->build();
         if ($isExists)
             $fail(
                 $this->getTranslator()->get('validation.unique', ['attribute' => $attribute])
             );
     }
 
-    public function getExcepts(): array
+    /**
+     * @return ExistableBridge
+     */
+    public function getExistableBridge(): ExistableBridge
     {
-        return $this->excepts;
+        return $this->existableBridge;
     }
 
-    public function setExcepts(array $excepts): void
+    /**
+     * @param ExistableBridge $existableBridge
+     */
+    public function setExistableBridge(ExistableBridge $existableBridge): void
     {
-        $this->excepts = $excepts ;
+        $this->existableBridge = $existableBridge;
     }
 
-    public function getColumnName(): string|null
-    {
-        if (!isset($this->columnName))
-            return null;
-    }
-
-    public function setColumnName(string $columnName): void
-    {
-        $this->columnName = $columnName;
-    }
-
-    public function getTableName(): string
-    {
-        if (!isset($this->tableName))
-            throw new \Exception('you should use setTableName() method and fill table name property !!!');
-        return $this->tableName;
-    }
-
-    public function setTableName(string $tableName): void
-    {
-        $this->tableName = $tableName;
-    }
 
 
 }
