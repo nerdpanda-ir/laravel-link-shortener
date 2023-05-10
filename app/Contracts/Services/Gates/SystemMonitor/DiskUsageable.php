679c17f17783b5b93839188a7c7704b6e0a2cbaa
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 17:52:40 +0200

    [files]{ DiskUsageableGateContract.php } => rename to DiskUsageable.php

--- app/Contracts/Services/Gates/SystemMonitor/DiskUsageable.php
+++ app/Contracts/Services/Gates/SystemMonitor/DiskUsageable.php
@@ -4,7 +4,7 @@ namespace App\Contracts\Services\Gates\SystemMonitor;
 
 use App\Contracts\Model\Userable;
 
-interface DiskUsageableGateContract
+interface DiskUsageable
 {
     public function diskUsage(Userable $user):bool;
 }
