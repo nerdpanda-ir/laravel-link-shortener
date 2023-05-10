df389e549b5bed91aeecc1525ce6599b482a449b
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 11:00:20 +0200

    [file]{ Logger.php } -> interface extends from other contracts !!!

--- app/Contracts/Observers/Permission/Logger.php
+++ app/Contracts/Observers/Permission/Logger.php
@@ -2,10 +2,15 @@
 
 namespace App\Contracts\Observers\Permission;
 
+use App\Contracts\Observers\Permission\Logger\Createdable;
+use App\Contracts\Observers\Permission\Logger\Deletedable;
+use App\Contracts\Observers\Permission\Logger\Updatedable;
 use App\Contracts\TranslatorGetterable;
 use App\LoggerGetterable;
 
-interface Logger extends TranslatorGetterable , LoggerGetterable
+interface Logger extends
+    TranslatorGetterable , LoggerGetterable , Deletedable , Updatedable ,
+    Createdable
 {
 
 }
