c03e41c5c72a20cdbe98fe108cbe6035c7358470
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 15:40:30 +0200

    [file]{ sidebar-heading.blade.php }-> replace side bar header codes with component !!!

--- resources/views/components/partials/dashboard/nav.blade.php
+++ resources/views/components/partials/dashboard/nav.blade.php
@@ -23,12 +23,7 @@
             </li>
         </ul>
         @canany(['create-permission','view-all-permissions'])
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-                <span>Permissions</span>
-                <a class="link-secondary" href="#" aria-label="Add a new report">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle align-text-bottom" aria-hidden="true"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-                </a>
-            </h6>
+            <x-partials.sidebar-heading name="Permission" uri="{{route('dashboard.permission.create')}}"/>
             <ul class="nav flex-column mb-2">
                 @can('view-all-permissions')
                     <li class="nav-item">
