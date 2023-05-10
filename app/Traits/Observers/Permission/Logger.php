35f8f9ad4dbafaf4b19841dc0156ae2689a7a679
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 09:03:07 +0200

    [file]{ Logger.php } -> create trait for logger observer !!!

---
+++ app/Traits/Observers/Permission/Logger.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Traits\Observers\Permission;
+
+use App\Traits\LoggerGetterable;
+use App\Traits\TranslatorGetterable;
+
+trait Logger
+{
+    use TranslatorGetterable , LoggerGetterable;
+}
