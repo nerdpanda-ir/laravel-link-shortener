07cb19ceff90c7b781994210e3999fc65e81cf70
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 16:15:34 +0200

    [file]{ Permission.php } => use permission trait

--- app/Services/Gates/Permission.php
+++ app/Services/Gates/Permission.php
@@ -3,9 +3,9 @@
 namespace App\Services\Gates;
 use App\Contracts\Services\Gates\Permission as Contract;
 use App\Services\GateBase;
-
+use App\Traits\Services\Gates\Permission as PermissionTrait;
 
 class Permission extends GateBase implements Contract
 {
-
+    use PermissionTrait;
 }
