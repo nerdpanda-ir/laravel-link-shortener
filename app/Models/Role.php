e37d985a1437c2680d07f6f86af13dda82123959
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 18:59:18 +0200

    [file]{ Role.php } => make relation with creator and users for role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Contracts\Model\Role as Contract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model implements Contract
@@ -12,5 +13,11 @@ class Role extends Model implements Contract
     use HasFactory;
     public function permissions():BelongsToMany {
         return $this->belongsToMany(Permission::class);
+    }
+    public function creator():BelongsTo {
+        return $this->belongsTo(User::class , 'created_by');
+    }
+    public function users():BelongsToMany {
+        return $this->belongsToMany(User::class);
     }
 }
