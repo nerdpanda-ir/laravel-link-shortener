d659c94401615402fc996355b224470b098214c9
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 17:02:21 +0200

    [files]{ AuthServiceProvider.php } => replace permission gate with interface

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use App\Services\Gates\Permission;
+use App\Contracts\Services\Gates\Permission;
+use App\Contracts\Services\Gates\Role;
 use App\Services\Gates\SystemMonitor;
 use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
