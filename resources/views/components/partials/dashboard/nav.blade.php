37e2fb3ea2714411d5b9133cbdf23f18f5f56dca
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 15:48:50 +0200

    [file]{ nav.blade.php }-> replace nav items with component !!!

--- resources/views/components/partials/dashboard/nav.blade.php
+++ resources/views/components/partials/dashboard/nav.blade.php
@@ -26,22 +26,11 @@
             <x-partials.sidebar-heading name="Permission" uri="{{route('dashboard.permission.create')}}"/>
             <ul class="nav flex-column mb-2">
                 @can('view-all-permissions')
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('dashboard.permission.view-all')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-text-bottom" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            All
-                        </a>
-                    </li>
+                    <x-partials.sidebar-link-item title="All" uri="{{route('dashboard.permission.view-all')}}"/>
                 @endcan
                 @can('create-permission')
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('dashboard.permission.create')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-text-bottom" aria-hidden="true"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            Create
-                        </a>
-                    </li>
+                     <x-partials.sidebar-link-item title="Create" uri="{{route('dashboard.permission.create')}}"/>
                 @endcan
-
             </ul>
         @endcanany
     </div>
