20e454c1949e1ff57e13c4473b51e2c2663dc65a
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 11:29:14 +0200

    [file]{ TranslatorKeyGetter.php } -> rename to TranslateKeyGetter.php

---
+++ app/Services/TranslateKeyGetter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\Services\TranslateKeyGetter as Contract;
+class TranslateKeyGetter implements Contract
+{
+
+}
--- app/Services/TranslatorKeyGetter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services;
-use App\Contracts\Services\TranslatorKeyGetter as Contract;
-class TranslatorKeyGetter implements Contract
-{
-
-}
