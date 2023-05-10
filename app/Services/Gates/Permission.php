72c454a4bd4001891810a22d359a28cedde6af7e
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 16:14:59 +0200

    [file]{ Permission.php } => remove use many traits !!!

--- app/Services/Gates/Permission.php
+++ app/Services/Gates/Permission.php
@@ -3,12 +3,9 @@
 namespace App\Services\Gates;
 use App\Contracts\Services\Gates\Permission as Contract;
 use App\Services\GateBase;
-use App\Traits\Services\Gates\Permission\Createable;
-use App\Traits\Services\Gates\Permission\Deleteable;
-use App\Traits\Services\Gates\Permission\Editable;
-use App\Traits\Services\Gates\Permission\ViewAllable;
+
 
 class Permission extends GateBase implements Contract
 {
-    use ViewAllable , Createable , Deleteable , Editable ;
+
 }
