a253ac4e8d32e94db34f0e0646974969935bc0e1
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 16:45:00 +0200

    [files]{ AbstractCrudableGate.php } => modify used namespace

--- app/Contracts/Services/AbstractCrudableGate.php
+++ app/Contracts/Services/AbstractCrudableGate.php
@@ -2,10 +2,10 @@
 
 namespace App\Contracts\Services;
 
+use App\Contracts\Services\AbstractCrudableGate\ViewAllable;
 use App\Contracts\Services\AbstractCrudableGate\Createable;
-use App\Contracts\Services\AbstractCrudableGate\Deleteable;
 use App\Contracts\Services\AbstractCrudableGate\Editable;
-use App\Contracts\Services\AbstractCrudableGate\Viewallable;
+use App\Contracts\Services\AbstractCrudableGate\Deleteable;
 
 interface AbstractCrudableGate extends Viewallable , Createable , Editable , Deleteable
 {
