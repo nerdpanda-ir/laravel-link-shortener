95ac0ecfd758bbba2ff1028d88388702c41ab539
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 11:53:52 +0200

    [file]{ TranslateKeyGetter.php } -> implement method !!!

--- app/Services/TranslateKeyGetter.php
+++ app/Services/TranslateKeyGetter.php
@@ -4,5 +4,8 @@ namespace App\Services;
 use App\Contracts\Services\TranslateKeyGetter as Contract;
 class TranslateKeyGetter implements Contract
 {
-
+    public function modelLog(string $action, string $model , string $status): string
+    {
+        return "messages.log.$action.$model.$status";
+    }
 }
