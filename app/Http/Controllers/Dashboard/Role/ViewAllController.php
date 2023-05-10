d3ef97d4e9e52685003cd598f2101956a1b02f90
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 18:29:11 +0200

    [file]{ ViewAllController.php } => create Controller

---
+++ app/Http/Controllers/Dashboard/Role/ViewAllController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Role;
+
+use App\Http\Controllers\Controller;
+
+class ViewAllController extends Controller
+{
+    public function __invoke()
+    {
+        dump(__METHOD__);
+    }
+}
