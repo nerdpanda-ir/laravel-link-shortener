fd1c225e954389a0212444ed5642041319464127
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 16:19:37 +0200

    [file]{ Permission.php } => remove many use commands

--- app/Contracts/Services/Gates/Permission.php
+++ app/Contracts/Services/Gates/Permission.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Contracts\Services\Gates;
-use App\Contracts\GateCreateableContract as Createable;
-use App\Contracts\GateEditable as Editable;
 
 interface Permission extends
     Viewallable , Createable , Editable , Deleteable
