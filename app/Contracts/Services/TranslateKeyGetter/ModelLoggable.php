276b759fa2a1be2e60189736c45758a36180f8d8
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 11:53:14 +0200

    [file]{ ModelLoggable.php } -> add parameter to method

--- app/Contracts/Services/TranslateKeyGetter/ModelLoggable.php
+++ app/Contracts/Services/TranslateKeyGetter/ModelLoggable.php
@@ -4,5 +4,5 @@ namespace App\Contracts\Services\TranslateKeyGetter;
 
 interface ModelLoggable
 {
-    public function modelLog(string $action , string $model):string;
+    public function modelLog(string $action , string $model , string $status):string;
 }
