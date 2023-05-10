8ae14b65e9287db3b59020f9abd2584ca3841788
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-10 17:48:37 +0200

    [files]{ SystemMonitorGateServiceProvider.php } => move to Services/Gates/ Directory  and rename to SystemMonitorServiceProvider.php

--- app/Providers/Services/Gates/SystemMonitorServiceProvider.php
+++ app/Providers/Services/Gates/SystemMonitorServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Providers;
+namespace App\Providers\Services\Gates;
 
+use App\Contracts\Services\Gates\SystemMonitor as Contract;
+use App\Services\Gates\SystemMonitor as Entity;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
-use App\Services\Gates\SystemMonitor as Entity;
-use App\Contracts\Services\Gates\SystemMonitor as Contract;
 
-class SystemMonitorGateServiceProvider extends ServiceProvider implements DeferrableProvider
+class SystemMonitorServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Register services.
