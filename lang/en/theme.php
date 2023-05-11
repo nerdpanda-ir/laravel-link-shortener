f5f01635bc26d29c81a135b6da772530135d6a1a
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-11 06:38:27 +0200

    [file]{ theme.php } => create translate file for store theme translate items !!!

---
+++ lang/en/theme.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'dashboard' => [
+        'no-result-found' => 'no found any result for display here !!!' ,
+        'view-all'=> 'view all :Name (total :total )',
+        'pages'=> [
+            'permissions' => [
+                'view-all'=> [
+                    'title'=> "view all permissions (total : :total )" ,
+                    ''
+                ],
+            ],
+            'roles'=> [
+                'view-all' => [
+                    'body-title'=> 'Show All Roles ( Total :total)' ,
+                    'no-exist-any-role' => 'No Found Any Role for display !!!'
+                ]
+            ],
+        ],
+    ]
+];
