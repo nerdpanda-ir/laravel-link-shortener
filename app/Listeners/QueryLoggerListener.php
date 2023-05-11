ac9a030b6d86e52140752248d2fa67a215664ca5
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-11 15:05:19 +0200

    [file]{ QueryLoggerListener.php } => create listener

---
+++ app/Listeners/QueryLoggerListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Database\Events\QueryExecuted;
+use App\Contracts\LoggerGetterable;
+use App\Traits\LoggerGetterable as LoggerGetterableTrait;
+use Psr\Log\LoggerInterface as Logger;
+
+class QueryLoggerListener implements LoggerGetterable
+{
+    use LoggerGetterableTrait;
+    protected Logger $logger;
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function handle(QueryExecuted $event): void
+    {
+        $this->logger->debug('query Executed !!!' ,[$event]);
+        if (!config('queryExecuted.log'))
+            return;
+        $payload = [];
+        foreach ($event as $key=>$value)
+            if ($key!='sql')
+                $payload[$key] = $value;
+        $this->logger->debug('QUERY EXECUTED -> '.$event->sql , $payload);
+    }
+}
