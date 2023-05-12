702eb105275330e870bd225f033c6b8e0916cdd9
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-12 16:50:34 +0200

    [file]{ LoggerMiddleware.php } => use from trait

--- app/Services/LoggerMiddleware.php
+++ app/Services/LoggerMiddleware.php
@@ -5,9 +5,10 @@ namespace App\Services;
 use Illuminate\Contracts\Translation\Translator;
 use Psr\Log\LoggerInterface as Logger;
 use App\Contracts\Services\LoggerMiddleware as Contract;
-
+use App\Traits\Services\LoggerMiddleware as LoggerMiddlewareTrait;
 abstract class LoggerMiddleware implements Contract
 {
+    use LoggerMiddlewareTrait;
     protected Logger $logger;
     protected Translator $translator;
     public function __construct(Logger $logger , Translator $translator)
