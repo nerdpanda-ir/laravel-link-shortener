3e3c1543a2fbc4e59ecbf834b735f5c807a94a85
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-12 16:36:54 +0200

    [file]{ log.php } => create translate items

---
+++ lang/en/log.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'request_start' => 'Request Start' ,
+    'response_delivered' => 'Delivered Response To User' ,
+    'actions' => [
+        'create' => 'successfully store' ,
+        'updated' => 'successfully updated' ,
+        'delete' => 'successfully deleted',
+    ]
+];
