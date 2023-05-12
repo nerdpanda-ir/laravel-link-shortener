34a5cc1e528294b5c3bfaa4e11c4d51cf340b09e
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-12 16:53:25 +0200

    [file]{ RequestLoggerMiddleware.php } => extends from LoggerMiddleware and modify handle method

--- app/Http/Middleware/RequestLoggerMiddleware.php
+++ app/Http/Middleware/RequestLoggerMiddleware.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\LoggerMiddleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class RequestLoggerMiddleware
+class RequestLoggerMiddleware extends LoggerMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         try {
-            \Log::info(trans('log.request_start'),[
-                'ip'=> $request->ip() , 'method'=> $request->method() , 'route'=> $request->route()->getName() ,
-                'destination' => $request->fullUrl() , 'action'=> $request->route()->getAction() ,
-                'inputs' => $request->all() ,
-            ]);
+            $this->getLogger()->info(
+                $this->getTranslator()->get('log.request_start') ,[
+                    'ip'=> $request->ip() , 'method'=> $request->method() , 'route'=> $request->route()->getName() ,
+                    'destination' => $request->fullUrl() , 'action'=> $request->route()->getAction() ,
+                    'inputs' => $request->all() ,
+                ]
+            );
         }catch (\Throwable $exception) {
             report($exception);
         } finally {
