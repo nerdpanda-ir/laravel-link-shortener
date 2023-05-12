e378f64396e275a6e7a626e0cfe45117d42b8ef2
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-12 09:14:41 +0200

    [file]{ StoreController.php } => modify translate key

--- app/Http/Controllers/Dashboard/Permission/StoreController.php
+++ app/Http/Controllers/Dashboard/Permission/StoreController.php
@@ -53,7 +53,7 @@ class StoreController extends Controller
             );
         } catch (\Throwable $throwable){
             $logger->emergency(
-                $translator->get('exceptions.actions.store.permission.exception_throw') ,
+                $translator->get('exceptions.actions.exception_throw', ['action' => 'store permission']) ,
             );
             report($throwable);
             return $exceptionHappenResponseBuilder->build($request->only('name'));
