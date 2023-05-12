9d4eb8ba29928e101c7899eec24f6ab156778b44
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-12 09:16:16 +0200

    [file]{ exceptions.php } => modify translate

--- lang/en/exceptions.php
+++ lang/en/exceptions.php
@@ -2,6 +2,6 @@
 return [
     'no-dependency-found' => 'number of :dependency is not enough for seeding :seeder ' ,
     'actions' => [
-        'exception_throw' => 'has exception when :action '
+        'exception_throw' => 'has exception when :action ' ,
     ]
 ];
