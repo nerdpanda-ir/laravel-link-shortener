95a5d6d27c9934c0500dd6c33ce8749013d849cf
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-12 11:39:54 +0200

    [file]{ FailCrud.php }->  use from trait

--- app/Exceptions/FailCrud.php
+++ app/Exceptions/FailCrud.php
@@ -4,8 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Contracts\Exceptions\FailCrud as Contract;
+use App\Traits\Exceptions\FailCrud as FailCrudTrait;
 class FailCrud extends Exception implements Contract
 {
+    use FailCrudTrait;
     protected array $context = [];
-
 }
