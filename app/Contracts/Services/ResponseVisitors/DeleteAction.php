903510f02cb87d3074208ebb3951f886fb9822f7
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 17:47:53 +0200

    [file]{ DeleteAction.php } => create abstract methods in interface !!!

--- app/Contracts/Services/ResponseVisitors/DeleteAction.php
+++ app/Contracts/Services/ResponseVisitors/DeleteAction.php
@@ -2,7 +2,12 @@
 
 namespace App\Contracts\Services\ResponseVisitors;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 interface DeleteAction
 {
+    public function ok(RedirectResponse $response, string $item): RedirectResponse;
+    public function  fail(RedirectResponse $response, string $item): RedirectResponse;
+    public function ThrowException(RedirectResponse $response, string $item): RedirectResponse;
 
 }
