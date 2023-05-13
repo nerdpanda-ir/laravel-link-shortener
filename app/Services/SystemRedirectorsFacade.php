5a5e8b0701d2038fe3047a7290cee652f314de30
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 20:24:09 +0200

    [file]{ SystemRedirectorsFacade.php } => create service

---
+++ app/Services/SystemRedirectorsFacade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\Redirectors\Permission;
+use App\Contracts\Services\SystemRedirectorsFacade as Contract;
+use App\Traits\ApplicationGetterable;
+use Illuminate\Contracts\Foundation\Application;
+
+class SystemRedirectorsFacade implements Contract
+{
+    use ApplicationGetterable;
+    protected Application $application;
+    public function __construct(Application $application)
+    {
+        $this->application = $application ;
+    }
+
+    public function permission(): Permission
+    {
+        return $this->getApplication()
+                    ->make('contract.redirector.permission');
+    }
+}
