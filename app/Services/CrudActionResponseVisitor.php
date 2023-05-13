0cdb3c019b654e41902ef3eda7f02f6f1f43af5c
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 13:37:29 +0200

    [file]{ CrudActionResponseVisitor.php } => use Translator Getter trait and define translator property in class and fill translator from construct !!!

--- app/Services/CrudActionResponseVisitor.php
+++ app/Services/CrudActionResponseVisitor.php
@@ -2,7 +2,15 @@
 
 namespace App\Services;
 use App\Contracts\Services\CrudActionResponseVisitor as Contract;
+use App\Traits\TranslatorGetterable;
+use Illuminate\Contracts\Translation\Translator;
+
 abstract class CrudActionResponseVisitor implements Contract
 {
-
+    use TranslatorGetterable;
+    protected Translator $translator;
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
 }
