66c127f860a646ba531491a69bd70e236a15b83a
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 15:07:56 +0200

    [file]{ Crud.php } => modify parameter type

--- app/Contracts/Services/ResponseVisitors/Cruds.php
+++ app/Contracts/Services/ResponseVisitors/Cruds.php
@@ -3,11 +3,12 @@
 namespace App\Contracts\Services\ResponseVisitors;
 
 use App\Contracts\TranslatorGetterable;
+use Illuminate\Http\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 interface Cruds extends TranslatorGetterable
 {
-    public function ok(Response $response , string $item):Response;
-    public function fail(Response $response , string $item):Response;
-    public function ThrowException(Response $response , string $item ):Response;
+    public function ok(RedirectResponse $response , string $item):Response;
+    public function fail(RedirectResponse $response , string $item):Response;
+    public function ThrowException(RedirectResponse $response , string $item ):Response;
 }
