8d00abd75c29c6fa915437ce6ee77773ff4d698b
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 19:37:33 +0200

    [file]{ Permission.php } => implement view all method

--- app/Http/Redirector/Permission.php
+++ app/Http/Redirector/Permission.php
@@ -4,7 +4,13 @@ namespace App\Http\Redirector;
 
 use App\Services\Redirector;
 use App\Contracts\Redirectors\Permission as Contract;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 class Permission extends Redirector implements  Contract
 {
-
+    public function viewAll(): RedirectResponse
+    {
+        return $this->getRedirector()
+                    ->route('dashboard.permission.view-all');
+    }
 }
