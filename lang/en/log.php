e0cb80f8dc30b63e57d1e585c57f2b800b449176
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 08:46:39 +0200

    [file]{  log.php } => modify translate

--- lang/en/log.php
+++ lang/en/log.php
@@ -2,9 +2,18 @@
 return [
     'request_start' => 'Request Start' ,
     'response_delivered' => 'Delivered Response To User' ,
-    'actions' => [
-        'create' => 'successfully store' ,
-        'updated' => 'successfully updated' ,
-        'delete' => 'successfully deleted',
+    'crud' => [
+        'create' => [
+            'ok'=> 'successfully store' ,
+            'throw_exception' => 'has exception when create :item ' ,
+        ] ,
+        'updated' => [
+            'ok'=>'successfully updated' ,
+            'throw_exception' => 'has exception when create :item ' ,
+        ],
+        'delete' => [
+            'ok'=> 'successfully deleted' ,
+            'throw_exception' => 'has exception when create :item '
+        ],
     ]
 ];
