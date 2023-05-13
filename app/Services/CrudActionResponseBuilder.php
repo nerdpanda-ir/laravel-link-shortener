b2b7b9de0b0f2193fe15b6878b7ecb116a24ea40
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 14:48:31 +0200

    [file]{ CrudActionResponseBuilder.php } => move to response builders

--- app/Services/CrudActionResponseBuilder.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services;
-use App\Contracts\Services\CrudActionResponseBuilder as Contract ;
-abstract class CrudActionResponseBuilder implements Contract
-{
-
-}
---
+++ app/Services/ResponseBuilders/CrudActionResponseBuilder.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\ResponseBuilders;
+use App\Contracts\Services\ResponseBuilders\CrudActionResponseBuilder as Contract;
+
+abstract class CrudActionResponseBuilder implements Contract
+{
+
+}
