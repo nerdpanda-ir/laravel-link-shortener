04f38742e6adf752854a91276aeeb76cbb676369
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 19:34:44 +0200

    [file]{ Permission.php } => create redirector

---
+++ app/Http/Redirector/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Redirector;
+
+use App\Services\Redirector;
+use App\Contracts\Redirectors\Permission as Contract;
+class Permission extends Redirector implements  Contract
+{
+
+}
