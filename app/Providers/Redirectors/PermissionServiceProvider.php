a288c48eb32bdd4676295de0d2e9c7e5df301d37
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 19:40:58 +0200

    [file]{ PermissionServiceProvider.php } => create provider

---
+++ app/Providers/Redirectors/PermissionServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers\Redirectors;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+use App\Contracts\Redirectors\Permission as Contract;
+use App\Http\Redirector\Permission as Redirector;
+class PermissionServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public function register(): void
+    {
+        $this->app->singleton(Contract::class,Redirector::class);
+    }
+    public function provides():array
+    {
+        return [Contract::class];
+    }
+}
