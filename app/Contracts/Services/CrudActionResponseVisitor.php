ba5222c498125f757bed4a48bccf2b13655acb41
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 13:30:33 +0200

    [file]{ CrudResponseVisitor.php } =>  rename contract and service

--- app/Contracts/Services/CrudActionResponseVisitor.php
+++ app/Contracts/Services/CrudActionResponseVisitor.php
@@ -4,7 +4,7 @@ namespace App\Contracts\Services;
 
 use Symfony\Component\HttpFoundation\Response;
 
-interface CrudResponseVisitor
+interface CrudActionResponseVisitor
 {
     public function ok(Response $response):Response;
     public function fail(Response $response):Response;
---
+++ app/Services/CrudActionResponseVisitor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\Services\CrudActionResponseVisitor as Contract;
+abstract class CrudActionResponseVisitor implements Contract
+{
+
+}
