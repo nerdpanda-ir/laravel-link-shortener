9a3a45dbf96978a53808de3715eb13bcf3c97e12
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 21:03:07 +0200

    [file]{ DeleteController.php } -> modify controller

--- app/Http/Controllers/Dashboard/Permission/DeleteController.php
+++ app/Http/Controllers/Dashboard/Permission/DeleteController.php
@@ -4,17 +4,18 @@ namespace App\Http\Controllers\Dashboard\Permission;
 
 use App\Contracts\Exceptions\FailCrud;
 use App\Contracts\Model\Permission as Permission;
-use App\Contracts\Responses\Dashboard\Permission\Delete\Fail;
+use App\Contracts\Responses\Dashboard\Permission\Delete;
 use App\Contracts\Responses\Dashboard\Permission\Delete\Ok;
-use App\Contracts\Responses\Dashboard\Permission\Delete\ThrowException;
 use App\Contracts\Responses\Dashboard\Permission\NotFound;
+use App\Contracts\Services\ResponseVisitors\DeleteAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Translation\Translator;
 use Psr\Log\LoggerInterface as Logger;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use  App\Contracts\Redirectors\Permission as PermissionRedirector;
+use App\Contracts\Services\ResponseVisitors\NotFound as NotFoundVisitor;
 class DeleteController extends Controller
 {
 
@@ -22,11 +23,10 @@ class DeleteController extends Controller
      * @param \App\Models\Permission $model
      */
     public function __invoke(
-        string $id , string $name , Permission $model ,
-        ExceptionHandler $exceptionHandler , FailCrud $failCrud,
-        Logger $logger , Translator $translator , ThrowException $throwExceptionResponseBuilder ,
-        NotFoundHttpException $notFoundHttpException , NotFound $notFoundResponseBuilder ,
-        Ok $okResponseBuilder , Fail $failDeleteResponseBuilder
+        string $id , string $name , Permission $model, Logger $logger , Translator $translator ,
+        ExceptionHandler $exceptionHandler , FailCrud $failCrud, DeleteAction $responseVisitor ,
+        NotFoundHttpException $notFoundHttpException , NotFoundVisitor $notFoundVisitor ,
+        PermissionRedirector $redirector ,
     ):RedirectResponse
     {
         try {
@@ -36,14 +36,14 @@ class DeleteController extends Controller
             $deleted = $permission->delete();
             if (!$deleted)
                 throw $failCrud;
-            return $okResponseBuilder->build($permission->name);
+            return $responseVisitor->ok($redirector->viewAll(),'permission '.$permission->name);
         }catch (NotFoundHttpException $exception ){
-            return $notFoundResponseBuilder->build($name);
+            return $notFoundVisitor->visit($redirector->viewAll(),"permission {$permission->name}");
         }catch (FailCrud $exception){
             $exception->setMessage($translator->get('log.crud.delete.fail', ['item' => 'permission']));
             $exception->setContext(['permission_id'=> $permission->id, 'permission_name'=> $permission->name]);
             $exceptionHandler->report($exception);
-            return $failDeleteResponseBuilder->build($permission->name);
+            return $responseVisitor->fail($redirector->viewAll() ,'permission '.$permission->name);
         }catch (\Throwable $exception){
             $finalName = $permission->name ?? $name;
             $logger->emergency(
@@ -51,7 +51,9 @@ class DeleteController extends Controller
                 ['id' => $id]
             );
             $exceptionHandler->report($exception);
-            return $throwExceptionResponseBuilder->build($finalName);
+            return $responseVisitor->ThrowException(
+                $redirector->viewAll(),'permission with name : '.$finalName
+            );
         }
     }
 }
