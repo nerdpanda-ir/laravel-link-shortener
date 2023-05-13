b3e3dd5efd9761b6943325b9cc2fa1a4f4310738
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-13 20:41:22 +0200

    [file]{ NotFound.php }-> create service

---
+++ app/Services/ResponseVisitors/NotFound.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\ResponseVisitors;
+use App\Contracts\Services\ResponseVisitors\NotFound as Contract;
+use App\Services\ResponseBuilder;
+use App\Traits\TranslatorGetterable;
+use Illuminate\Contracts\Translation\Translator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class NotFound implements Contract
+{
+    use TranslatorGetterable;
+    protected Translator $translator;
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator ;
+    }
+
+    public function visit(Response $response , string $item): RedirectResponse
+    {
+        return $response->with('system.messages.error',[
+            $this->getTranslator()->get('messages.not_found', ['item' => $item ])
+        ]);
+    }
+}
