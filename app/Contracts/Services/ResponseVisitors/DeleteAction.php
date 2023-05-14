18da63e6809d25ce11817ab37cde1352f546d349
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 07:37:35 +0200

    [file]{ DeleteAction.php } -> delete method in interface and extends from interface

--- app/Contracts/Services/ResponseVisitors/DeleteAction.php
+++ app/Contracts/Services/ResponseVisitors/DeleteAction.php
@@ -4,10 +4,8 @@ namespace App\Contracts\Services\ResponseVisitors;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-interface DeleteAction
+interface DeleteAction extends ExceptionThrowable
 {
     public function ok(RedirectResponse $response, string $item): RedirectResponse;
     public function  fail(RedirectResponse $response, string $item): RedirectResponse;
-    public function ThrowException(RedirectResponse $response, string $item): RedirectResponse;
-
 }
