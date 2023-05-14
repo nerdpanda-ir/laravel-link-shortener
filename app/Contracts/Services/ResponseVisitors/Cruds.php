a6d06b3ea24199841ea0ea1af947f35ea1be9832
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 18:20:23 +0200

    [file]{ Cruds.php } -> delete abstract methods and extends from interfaces !!!

--- app/Contracts/Services/ResponseVisitors/Cruds.php
+++ app/Contracts/Services/ResponseVisitors/Cruds.php
@@ -6,9 +6,7 @@ use App\Contracts\TranslatorGetterable;
 use Illuminate\Http\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-interface Cruds extends TranslatorGetterable
+interface Cruds extends TranslatorGetterable , Okable , Failable , ExceptionThrowable
 {
-    public function ok(RedirectResponse $response , string $item):Response;
-    public function fail(RedirectResponse $response , string $item):Response;
-    public function ThrowException(RedirectResponse $response , string $item ):Response;
+
 }
