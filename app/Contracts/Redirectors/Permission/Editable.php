45389d6277e899e7ad6d6cb33620ca005a099d07
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 17:13:06 +0200

    [files]{ Editable.php } -> create abstract contract

--- app/Contracts/Redirectors/Permission/Editable.php
+++ app/Contracts/Redirectors/Permission/Editable.php
@@ -2,7 +2,9 @@
 
 namespace App\Contracts\Redirectors\Permission;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 interface Editable
 {
-
+    public function edit(string $id , string $name , array $inputs = []):RedirectResponse;
 }
