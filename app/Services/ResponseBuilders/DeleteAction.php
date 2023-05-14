a2b7ffbd130cf9075832c53692c3ec51c9ca9678
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 08:57:42 +0200

    [file]{ DeleteAction.php } -> delete

--- app/Services/ResponseBuilders/DeleteAction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services\ResponseBuilders;
-use App\Contracts\Services\ResponseBuilders\DeleteAction as Contract;
-abstract class DeleteAction extends Cruds implements Contract
-{
-
-}
