401aab60b740602bd580ad21d27639de54e281a7
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 19:17:05 +0200

    [file]{ UpdateController.php } -> modify controller

--- app/Http/Controllers/Dashboard/Permission/UpdateController.php
+++ app/Http/Controllers/Dashboard/Permission/UpdateController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Dashboard\Permission;
 use App\Contracts\Exceptions\FailCrud as FailUpdateException;
 use App\Contracts\Model\Permission as Permission;
 use App\Contracts\Requests\Dashboard\Permission\Update as Request;
-use App\Contracts\Responses\Dashboard\Permission\NotFound as NotFoundResponseBuilder;
 use App\Contracts\Responses\Dashboard\Permission\Update\ExceptionThrow;
 use App\Contracts\Responses\Dashboard\Permission\Update\Fail as FailResponseBuilder;
 use App\Contracts\Responses\Dashboard\Permission\Update\Ok as OkResponseBuilder;
+use App\Contracts\Services\ResponseVisitors\NotFound;
+use App\Contracts\Services\ResponseVisitors\UpdateAction;
 use App\Exceptions\FailCrud;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Debug\ExceptionHandler;
@@ -16,7 +17,7 @@ use Illuminate\Contracts\Translation\Translator;
 use Psr\Log\LoggerInterface as Logger;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use App\Contracts\Redirectors\Permission as Redirector;
 class UpdateController extends Controller
 {
     /**
@@ -27,10 +28,8 @@ class UpdateController extends Controller
      */
     public function __invoke(
         string $id , string $name , Request $request , Permission $model , Logger $logger , Translator $translator ,
-        ExceptionThrow $exceptionThrowResponseBuilder , FailResponseBuilder $failResponseBuilder ,
-        NotFoundHttpException $notFoundException , NotFoundResponseBuilder $notFoundResponseBuilder ,
-        ExceptionHandler $exceptionHandler , FailCrud $failUpdateException ,
-        OkResponseBuilder $okResponseBuilder
+        NotFound $notFoundVisitor , Redirector $redirector , UpdateAction $visitor , ExceptionHandler $exceptionHandler ,
+        FailCrud $failUpdateException , NotFoundHttpException $notFoundException ,
     ):RedirectResponse
     {
         try {
@@ -42,15 +41,21 @@ class UpdateController extends Controller
             $updated = $permission->update();
             if (!$updated)
                 throw $failUpdateException;
-            return $okResponseBuilder->build($permission->name);
+            return $visitor->ok(
+                $redirector->viewAll(),
+                "permission with id -> $id and name ->$permission->name "
+            );
         }catch (FailUpdateException $exception){
             $exception->setContext(['id' => $id , 'name' => $name]);
             $exception->setMessage($translator->get('log.crud.updated.fail', ['item' => 'permission']));
             $exceptionHandler->report($exception);
-            return $failResponseBuilder->build($id,$name,$request->only(['name']));
+            return $visitor->fail(
+                $redirector->edit($id,$name,$request->only(['name'])) ,
+                "permission with name $name" ,
+            );
         }
         catch (NotFoundHttpException $exception){
-            return $notFoundResponseBuilder->build($name);
+            return $notFoundVisitor->visit($redirector->viewAll(),'permission with name -> '.$name);
         }catch (\Throwable $exception){
             $finalName = $permission->name ?? $name;
             $logger->emergency(
@@ -58,7 +63,10 @@ class UpdateController extends Controller
                 ['id' => $id , 'name' => $finalName]
             );
             $exceptionHandler->report($exception);
-            return $exceptionThrowResponseBuilder->build($id,$finalName,$request->only(['name']));
+            return $visitor->throwException(
+                $redirector->edit($id , $finalName , $request->only('name')) ,
+                "permission $finalName"
+            );
         }
     }
 }
