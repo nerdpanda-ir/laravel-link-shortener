13c39f5d5548ee783935ea45961ab8bfd5562ff0
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 16:30:29 +0200

    [files] -> delete useless classes

--- app/Contracts/Responses/Dashboard/Permission/Store/FailStoreBuilder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Contracts\Responses\Dashboard\Permission\Store;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
-interface FailStoreBuilder
-{
-    public function build(array $inputs , string $errorMessage):RedirectResponse;
-}
