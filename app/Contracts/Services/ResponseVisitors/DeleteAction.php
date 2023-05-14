dbd32351c354453abe2c09d122acca363dead3a3
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 11:19:39 +0200

    [file]{ DeleteAction.php } -> delete abstract method and extends from interface

--- app/Contracts/Services/ResponseVisitors/DeleteAction.php
+++ app/Contracts/Services/ResponseVisitors/DeleteAction.php
@@ -4,8 +4,8 @@ namespace App\Contracts\Services\ResponseVisitors;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-interface DeleteAction extends ExceptionThrowable
+interface DeleteAction extends Okable , ExceptionThrowable
 {
-    public function ok(RedirectResponse $response, string $item): RedirectResponse;
+
     public function  fail(RedirectResponse $response, string $item): RedirectResponse;
 }
