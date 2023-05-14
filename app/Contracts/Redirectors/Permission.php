2a50a6beef1bb1aca2cf84cabfca59e58afb1239
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-14 09:16:10 +0200

    [file]{ Permission.php } -> extends from Createable interface

--- app/Contracts/Redirectors/Permission.php
+++ app/Contracts/Redirectors/Permission.php
@@ -2,10 +2,11 @@
 
 namespace App\Contracts\Redirectors;
 
+use App\Contracts\Redirectors\Permission\Createable;
 use App\Contracts\Redirectors\Permission\Editable;
 use App\Contracts\Redirectors\Permission\ViewAllable;
 
-interface Permission extends ViewAllable , Editable
+interface Permission extends ViewAllable , Editable , Createable
 {
 
 }
