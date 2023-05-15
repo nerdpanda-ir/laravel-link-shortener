7b3f0982b2b6a0978a9db77601df90e8e5751b81
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-15 13:08:24 +0200

    [file]{ login.php } -> extends from interface and remove old interface

--- app/Contracts/Events/Login.php
+++ app/Contracts/Events/Login.php
@@ -2,9 +2,10 @@
 
 namespace App\Contracts\Events;
 
+use App\Contracts\Userable;
 use App\Contracts\UserGetterable;
 
-interface Login extends UserGetterable
+interface Login extends Userable
 {
 
 }
