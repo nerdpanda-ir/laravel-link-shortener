6ac25c026c9f034bac87ae5a7cddc07ddc0efe38
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-15 13:52:38 +0200

    [file]{ DoLoginController.php } -> dispatch login event

--- app/Http/Controllers/DoLoginController.php
+++ app/Http/Controllers/DoLoginController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Contracts\Events\Login;
 use App\Contracts\Requests\DoLogin as RequestContract;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Routing\Redirector;
 use Psr\Log\LoggerInterface;
@@ -12,7 +14,8 @@ use Symfony\Component\HttpFoundation\Response;
 class DoLoginController extends Controller
 {
     public function __invoke(
-        RequestContract $request , Auth $auth , LoggerInterface $logger , Redirector $redirector , Translator $translator
+        RequestContract $request , Auth $auth , LoggerInterface $logger , Redirector $redirector ,
+        Translator $translator , Dispatcher $eventDispatcher , Login $loginEvent
     ):Response
     {
         try {
@@ -25,6 +28,8 @@ class DoLoginController extends Controller
                         ->withInput($request->only('email','password','remember'));
 
             $user = $auth->guard('web')->user();
+            $loginEvent->setUser($user);
+            $eventDispatcher->dispatch($loginEvent);
             return $redirector->to(RouteServiceProvider::HOME)
                     ->with('system.messages.ok',[
                         $translator->get('messages.auth.login.ok',['name' => $user->name])
