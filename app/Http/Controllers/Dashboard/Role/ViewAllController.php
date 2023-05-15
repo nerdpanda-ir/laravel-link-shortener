da12741a2c1640546cfe143a76ddee7c0c94fcca
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-15 15:01:43 +0200

    [file]{ ViewAllController.php } -> exception handler

--- app/Http/Controllers/Dashboard/Role/ViewAllController.php
+++ app/Http/Controllers/Dashboard/Role/ViewAllController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers\Dashboard\Role;
 
+use App\Contracts\Redirectors\Dashboard;
+use App\Contracts\Services\DateService;
+use App\Contracts\Services\ResponseVisitors\ViewAllAction;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\View\View;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Psr\Log\LoggerInterface as Logger;
+use Illuminate\Contracts\Translation\Translator;
 
 class ViewAllController extends Controller
 {
-    public function __invoke()
+    public function __invoke(
+        ExceptionHandler $exceptionHandler , Logger $logger , Translator $translator ,
+        Dashboard $dashboardRedirector , DateService $dateService , ViewAllAction $viewAllActionVisitor
+    ):View|RedirectResponse
     {
-        dump(__METHOD__);
+        try {
+            0/0;
+        }catch (\Throwable $exception){
+            $logger->emergency(
+                $translator->get('log.crud.view_all.throw_exception', ['item' => 'roles'])
+            );
+            $exceptionHandler->report($exception);
+            return $viewAllActionVisitor->throwException($dashboardRedirector->redirect(),'roles');
+        }
     }
 }
