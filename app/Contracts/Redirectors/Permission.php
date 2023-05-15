5c588c973d2c693157b8f619b683a90df35d40a1
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-15 16:34:34 +0200

    [file]{ Permission.php } -> remove used interface

--- app/Contracts/Redirectors/Permission.php
+++ app/Contracts/Redirectors/Permission.php
@@ -4,7 +4,6 @@ namespace App\Contracts\Redirectors;
 
 use App\Contracts\Redirectors\Permission\Createable;
 use App\Contracts\Redirectors\Permission\Editable;
-use App\Contracts\Redirectors\Permission\ViewAllable;
 
 interface Permission extends ViewAllable , Editable , Createable
 {
