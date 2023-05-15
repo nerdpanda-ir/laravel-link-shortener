3b761842ecdfa24ace5d99f7f4e05ab7fc552eef
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-15 16:42:35 +0200

    [file]{ Role.php } -> create redirector

---
+++ app/Http/Redirector/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Redirector;
+use App\Contracts\Redirectors\Role as Contract;
+use App\Services\Redirector;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class Role extends Redirector implements Contract
+{
+    public function viewAll(): RedirectResponse
+    {
+        return $this->getRedirector()->route('dashboard.role.view-all');
+    }
+}
