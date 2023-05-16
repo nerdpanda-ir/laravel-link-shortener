a951e205dce639b4904136685a1f85ece89ff631
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-16 09:30:53 +0200

    [files]{ CreateAction.php } -> make visitor

---
+++ app/Providers/Services/ResponseVisitors/CreateAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers\Services\ResponseVisitors;
+use App\Contracts\Services\ResponseVisitors\CreateAction as Contract;
+use App\Traits\TranslatorGetterable;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Illuminate\Contracts\Translation\Translator;
+class CreateAction implements Contract
+{
+    use TranslatorGetterable;
+    protected Translator $translator;
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function throwException(RedirectResponse $response, string $item): RedirectResponse
+    {
+        return $response->with('system.messages.error',[
+            $this->getTranslator()->get(
+                'messages.crud.create.throw_exception',
+                ['date' => date('Y-m-d hiH:i:s'), 'item' => $item ]
+            )
+        ]);
+    }
+}
