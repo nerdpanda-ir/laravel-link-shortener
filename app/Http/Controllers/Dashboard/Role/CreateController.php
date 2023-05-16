15608c20739fe275f79a52c9df2aefe00d60e534
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-16 11:40:40 +0200

    [files]{ CreateController.php } -> complete create controller

--- app/Http/Controllers/Dashboard/Role/CreateController.php
+++ app/Http/Controllers/Dashboard/Role/CreateController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers\Dashboard\Role;
 
+use App\Contracts\Model\Permission;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\View\View;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Psr\Log\LoggerInterface as Logger;
+use Illuminate\Contracts\Translation\Translator ;
+use App\Http\Redirector\Role as Redirector;
+use App\Contracts\Services\ResponseVisitors\CreateAction as ResponseVisitor;
+use Illuminate\Contracts\View\Factory as ViewFactory;
 
 class CreateController extends Controller
 {
-    public function __invoke()
+
+    /**
+     * @param \App\Models\Permission $permission
+     * @throws \Throwable
+     */
+    public function __invoke(
+        Logger $logger , ExceptionHandler $exceptionHandler , Translator $translator , Redirector $redirector ,
+        ResponseVisitor $responseVisitor , ViewFactory $viewFactory , Permission $permission
+    ):View|RedirectResponse
     {
-        dump(__METHOD__);
+        try {
+            $permissions = $permission->latest()->get(['id','name']);
+            return $viewFactory->make('page.dashboard.role.create')
+                               ->with('permissions',$permissions);
+        }catch (\Throwable $exception){
+            $logger->emergency(
+                $translator->get('log.crud.create.throw_exception', ['item' => 'role'])
+            );
+            $exceptionHandler->report($exception);
+            return $responseVisitor->throwException($redirector->viewAll(),'role');
+        }
     }
 }
