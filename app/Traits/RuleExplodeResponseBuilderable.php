30f7d61aba1e7cc56b805a5f4e4854f2876a3bd4
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-17 11:54:43 +0200

    [file]{ RuleExplodeResponseBuilderable.php  } -> create trait

---
+++ app/Traits/RuleExplodeResponseBuilderable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Traits;
+
+use App\Contracts\RuleExplodeResponseBuilder;
+
+trait RuleExplodeResponseBuilderable
+{
+    use RuleExplodeResponseBuilderGetterable , RuleExplodeResponseBuilderSetterable ;
+}
