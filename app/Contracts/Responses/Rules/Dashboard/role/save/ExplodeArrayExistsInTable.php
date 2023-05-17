cf218145418a46457dce935e311f8992c2699ff9
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-17 12:08:38 +0200

    [file]{ ExplodeArrayExistsInTable.php } -> create interface

---
+++ app/Contracts/Responses/Rules/Dashboard/role/save/ExplodeArrayExistsInTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Contracts\Responses\Rules\Dashboard\role\save;
+
+use App\Contracts\RuleExplodeResponseBuilder;
+
+interface ExplodeArrayExistsInTable extends RuleExplodeResponseBuilder
+{
+
+}
