faf18bedd02d1cf06096de7f272f9c1cf0a6bea1
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-17 13:09:39 +0200

    [file]{ FailRuleMessageBuilderable.php } -> create interface

---
+++ app/Contracts/FailRuleMessageBuilderable.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Contracts\Services\FailRuleMessageBuilder;
+
+interface FailRuleMessageBuilderable extends FailRuleMessageBuilderSetterable , FailRuleMessageBuilderGetterable
+{
+}
