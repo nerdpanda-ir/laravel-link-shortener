142ea97c77717404f0e7ca5f4d054fb462f0eeab
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-17 11:46:06 +0200

    [file]{ 2023_04_28_094928_create_roles_table.php  } -> modify role name length in database

--- database/migrations/2023_04_28_094928_create_roles_table.php
+++ database/migrations/2023_04_28_094928_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name',64)->unique();
             $table->unsignedBigInteger('created_by')->nullable();
             $table->timestamps();
 
