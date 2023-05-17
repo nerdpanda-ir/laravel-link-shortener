6fd1e6d9cb04d378483b886ccf11c77927e68e12
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-17 12:08:55 +0200

    [file]{ ExplodeArrayExistsInTable.php } -> create response builder

---
+++ app/Http/Responses/Rule/Dashboard/Role/ExplodeArrayExistsInTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Responses\Rule\Dashboard\Role;
+use App\Contracts\Responses\Rules\Dashboard\role\save\ExplodeArrayExistsInTable as Contract;
+use App\Services\ResponseBuilderContainTranslator;
+use Illuminate\Support\Collection;
+use Symfony\Component\HttpFoundation\Response;
+
+class ExplodeArrayExistsInTable extends ResponseBuilderContainTranslator implements Contract
+{
+    public function Build(?Collection $arguments = null): Response
+    {
+        return $this->getResponse()->redirectToRoute('dashboard.role.create')
+                    ->withInput($arguments->get('inputs'))
+                    ->with(
+                        'system.messages.error' , [
+                            $this->getTranslator()->get(
+                                'validation.custom.attribute-name.array_is_exists_in_table.throw_exception', [
+                                    'date' =>  $arguments->get('date') ,
+                                    'attribute' => $arguments->get('attribute')
+                                ]
+                            )
+                        ]
+                    );
+    }
+}
