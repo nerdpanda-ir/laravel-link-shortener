1423fe8faa6f06e1cf75076cfb87f7622160f8e9
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-17 13:17:35 +0200

    [file]{ ArrayIsExistsInTable.php } -> extends from contract

--- app/Contracts/Rule/ArrayIsExistsInTable.php
+++ app/Contracts/Rule/ArrayIsExistsInTable.php
@@ -5,13 +5,15 @@ namespace App\Contracts\Rule;
 use App\Contracts\DatabaseManagerGetterable;
 use App\Contracts\DateServiceGetterable;
 use App\Contracts\ExceptionHandlerGetterable;
+use App\Contracts\FailRuleMessageBuilderable;
 use App\Contracts\LoggerGetterable;
 use App\Contracts\RuleExplodeResponseBuilderable;
 use App\Contracts\RuleExplodeResponseBuilderGetterable;
 use App\Contracts\TranslatorGetterable;
 
 interface ArrayIsExistsInTable extends  DatabaseManagerGetterable , TranslatorGetterable , LoggerGetterable ,
-    ExceptionHandlerGetterable , DateServiceGetterable , RuleExplodeResponseBuilderable
+    ExceptionHandlerGetterable , DateServiceGetterable , RuleExplodeResponseBuilderable ,
+    FailRuleMessageBuilderable
 {
     public function getTable(): string;
     public function setTable(string $table): void;
