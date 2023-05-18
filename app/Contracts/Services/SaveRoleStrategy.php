d738e93ea26fb2400883e37c154742757db02e28
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-18 15:00:51 +0200

    [file]{ SaveRoleStrategy.php } -> create contract

--- app/Contracts/Services/SaveRoleStrategy.php
+++ app/Contracts/Services/SaveRoleStrategy.php
@@ -2,9 +2,21 @@
 
 namespace App\Contracts\Services;
 
+use App\Contracts\AuthManagerGetterable;
+use App\Contracts\DateServiceGetterable;
+use App\Contracts\ExceptionHandlerGetterable;
+use App\Contracts\FailCrudExceptionGetterable;
+use App\Contracts\LoggerGetterable;
+use App\Contracts\RequestGetterable;
+use App\Contracts\ResponseVisitorGetterable;
+use App\Contracts\RoleModelGetterable;
+use App\Contracts\Services\Redirector\RedirectorGetterable;
+use App\Contracts\TranslatorGetterable;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-interface SaveRoleStrategy
+interface SaveRoleStrategy extends LoggerGetterable , TranslatorGetterable , RequestGetterable ,
+    ExceptionHandlerGetterable , ResponseVisitorGetterable , RedirectorGetterable , RoleModelGetterable ,
+    FailCrudExceptionGetterable , AuthManagerGetterable , DateServiceGetterable
 {
     public function save():RedirectResponse;
 }
