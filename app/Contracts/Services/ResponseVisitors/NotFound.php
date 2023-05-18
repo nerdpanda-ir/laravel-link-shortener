80aaa0e457aa8397f40d5b254314dd999472afcd
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-18 13:20:06 +0200

    [file]{ NotFound.php } -> extends from ResponseVisitor contract

--- app/Contracts/Services/ResponseVisitors/NotFound.php
+++ app/Contracts/Services/ResponseVisitors/NotFound.php
@@ -2,10 +2,11 @@
 
 namespace App\Contracts\Services\ResponseVisitors;
 
+use App\Contracts\Services\ResponseVisitor;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-interface NotFound
+interface NotFound extends ResponseVisitor
 {
     public function visit(Response $response , string $item):RedirectResponse;
 }
