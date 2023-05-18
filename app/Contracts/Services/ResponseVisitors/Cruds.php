a776c7060347efa30da547e9e7118a11439a72c3
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-18 13:18:13 +0200

    [file]{ Cruds.php } -> extends from ResponseVisitor contract

--- app/Contracts/Services/ResponseVisitors/Cruds.php
+++ app/Contracts/Services/ResponseVisitors/Cruds.php
@@ -2,12 +2,13 @@
 
 namespace App\Contracts\Services\ResponseVisitors;
 
+use App\Contracts\Services\ResponseVisitor;
 use App\Contracts\Services\ResponseVisitors\Statuses\ExceptionThrowable;
 use App\Contracts\Services\ResponseVisitors\Statuses\Failable;
 use App\Contracts\Services\ResponseVisitors\Statuses\Okable;
 use App\Contracts\TranslatorGetterable;
 
-interface Cruds extends TranslatorGetterable , Okable , Failable , ExceptionThrowable
+interface Cruds extends ResponseVisitor , TranslatorGetterable , Okable , Failable , ExceptionThrowable
 {
 
 }
