7552f344e758d98bf98c143e365a3a2b9d739e30
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-18 13:17:34 +0200

    [file]{ CreateAction.php } -> extends from ResponseVisitor contract

--- app/Contracts/Services/ResponseVisitors/CreateAction.php
+++ app/Contracts/Services/ResponseVisitors/CreateAction.php
@@ -3,8 +3,8 @@
 namespace App\Contracts\Services\ResponseVisitors;
 
 use App\Contracts\Services\ResponseVisitors\Statuses\ExceptionThrowable;
-
-interface CreateAction extends ExceptionThrowable
+use App\Contracts\Services\ResponseVisitor ;
+interface CreateAction extends ResponseVisitor , ExceptionThrowable
 {
 
 }
