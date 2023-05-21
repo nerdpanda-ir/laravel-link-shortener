a78e862020a7866466593cd97fbe4f82b91d0580
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-21 07:54:53 +0200

    [file]{ data.sql } => create data sql

---
+++ database/consoles/data.sql
@@ -0,0 +1,33 @@
+delete from `permissions`;
+delete from `permission_user`;
+insert into permissions values
+                            (null , 'permission-view-all'  , 1 , now() , null ) ,
+                            (null , 'permission-create'  , 1 , now() , null ) ,
+                            (null , 'permission-edit'  , 1 , now() , null ) ,
+                            (null , 'permission-delete'  , 1 , now() , null ),
+                            (null , 'role-view-all'  , 1 , now() , null ) ,
+                            (null , 'role-edit'  , 1 , now() , null ) ,
+                            (null , 'role-delete'  , 1 , now() , null ),
+                            (null , 'role-create'  , 1 , now() , null );
+insert into `permission_user` values
+                                  (
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-view-all' ) , 1 , now() , null
+                                  ),
+                                  (
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-create' ) , 1 , now() , null
+                                  ),
+                                  (
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-edit' ) , 1 , now() , null
+                                  ),
+                                  (
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-delete' ) , 1 , now() , null
+                                  ),
+                                  (
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='role-view-all' ) , 1 , now() , null
+                                  ),(
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='role-edit' ) , 1 , now() , null
+                                  ),(
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='role-delete' ) , 1 , now() , null
+                                  ),(
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='role-create' ) , 1 , now() , null
+                                  );
