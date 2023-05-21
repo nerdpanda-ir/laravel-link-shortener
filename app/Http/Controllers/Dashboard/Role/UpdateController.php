5553b7eac09c3cc1e88a2d4c79f909a3bcdfa4f0
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-21 08:08:54 +0200

    [file]{ UpdateController.php } => create controller for update role

---
+++ app/Http/Controllers/Dashboard/Role/UpdateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Role;
+
+use App\Http\Controllers\Controller;
+
+class UpdateController extends Controller
+{
+    public function __invoke()
+    {
+
+    }
+}
