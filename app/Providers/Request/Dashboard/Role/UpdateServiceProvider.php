ec3016511239931d6068bdf0daa11c09c7a4c084
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-21 10:06:36 +0200

    [file]{ UpdateServiceProvider.php } => create provider

---
+++ app/Providers/Request/Dashboard/Role/UpdateServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers\Request\Dashboard\Role;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+use App\Contracts\Requests\Dashboard\Role\Update as Contract;
+use App\Http\Requests\Dashboard\Role\Update as Request;
+class UpdateServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(Contract::class,Request::class);
+    }
+
+    public function provides():array
+    {
+        return [Contract::class];
+    }
+}
