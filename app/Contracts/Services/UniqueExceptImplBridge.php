c83cceb2789b927332adc7d97d791c508804bb63
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-21 15:23:32 +0200

    [file]{ UniqueExceptImplBridge.php } => modify  return type for setter methods

--- app/Contracts/Services/UniqueExceptImplBridge.php
+++ app/Contracts/Services/UniqueExceptImplBridge.php
@@ -5,13 +5,13 @@ namespace App\Contracts\Services;
 interface UniqueExceptImplBridge
 {
     public function getExcepts():array;
-    public function setExcepts(array $excepts);
+    public function setExcepts(array $excepts):self;
     public function getOnly():string;
-    public function setOnly(string $only):void;
+    public function setOnly(string $only):self;
     public function getTableName():string;
-    public function setTableName(string $tableName):void;
+    public function setTableName(string $tableName):self;
     public function getExceptColumn():string;
-    public function setExceptColumn(string $column):void;
+    public function setExceptColumn(string $column):self;
     public function getOnlyColumn():string;
-    public function setOnlyColumn(string $column):void;
+    public function setOnlyColumn(string $column):self;
 }
