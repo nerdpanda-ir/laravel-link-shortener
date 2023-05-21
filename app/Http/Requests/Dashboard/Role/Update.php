296230b8b85d8edf05cb4d0d067c7b7f416e6297
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-21 09:56:43 +0200

    [file]{ Update.php } => create update request for role update

---
+++ app/Http/Requests/Dashboard/Role/Update.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Dashboard\Role;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Contracts\Requests\Dashboard\Role\Update as Contract;
+class Update extends FormRequest implements Contract
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
