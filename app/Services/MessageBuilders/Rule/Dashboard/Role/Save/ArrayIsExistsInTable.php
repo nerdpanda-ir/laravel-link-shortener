b46b0417044a0c26debcd33b78952037bdb0e9b1
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-22 06:27:30 +0200

    [file]{ ArrayIsExistsInTable.php } => modify contract namespace

--- app/Services/MessageBuilders/Rule/Dashboard/Role/Save/ArrayIsExistsInTable.php
+++ app/Services/MessageBuilders/Rule/Dashboard/Role/Save/ArrayIsExistsInTable.php
@@ -2,9 +2,10 @@
 
 namespace App\Services\MessageBuilders\Rule\Dashboard\Role\Save;
 
+use App\Contracts\Services\MessageBuilders\Rule\Dashboard\Role\ArrayIsExistsInTable as Contract;
 use App\Services\FailRuleMessageBuilder;
 use Illuminate\Support\Collection;
-use App\Contracts\Services\MessageBuilders\Rule\Dashboard\Role\Save\ArrayIsExistsInTable as Contract;
+
 class ArrayIsExistsInTable extends FailRuleMessageBuilder implements Contract
 {
     public function build(?Collection $payload = null): string
