c64f5d6a8a110f5f03dac0602d77c7853a8cf4ce
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-22 19:05:24 +0200

    [file]{ Save.php}-> compatible form request with new arrayIsExistsInTableRule approach

--- app/Http/Requests/Dashboard/Role/Save.php
+++ app/Http/Requests/Dashboard/Role/Save.php
@@ -3,12 +3,12 @@
 namespace App\Http\Requests\Dashboard\Role;
 
 use App\Contracts\Requests\Dashboard\Role\Save as Contract;
-use App\Contracts\Responses\Rules\Dashboard\role\save\ExplodeArrayExistsInTableBridge;
 use App\Contracts\Rule\ArrayIsExistsInTable;
 use App\Contracts\Services\MessageBuilders\Rule\Dashboard\Role\ArrayIsExistsInTable as MessageBuilder;
 use Illuminate\Contracts\Auth\Factory;
 use Illuminate\Foundation\Http\FormRequest;
-
+use App\Http\Redirector\Role as Redirector;
+use App\Contracts\Services\ResponseVisitors\Rule\ExplodeArrayExistsInTable as ResponseVisitor;
 class Save extends FormRequest implements Contract
 {
     /**
@@ -27,14 +27,17 @@ class Save extends FormRequest implements Contract
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
     public function rules(
-        ArrayIsExistsInTable $arrayIsExistsInTableRule , ExplodeArrayExistsInTableBridge $explodeRuleResponseBuilder ,
-        MessageBuilder $messageBuilder ,
+        ArrayIsExistsInTable $arrayIsExistsInTableRule , MessageBuilder $messageBuilder ,
+        Redirector $redirector , ResponseVisitor $responseVisitor ,
     ): array
     {
         $arrayIsExistsInTableRule->setTable('permissions');
         $arrayIsExistsInTableRule->setColumn('name');
-        $arrayIsExistsInTableRule->setExplodeResponseBuilder($explodeRuleResponseBuilder);
         $arrayIsExistsInTableRule->setFailMessage($messageBuilder);
+        $arrayIsExistsInTableRule->setExplodeResponse(
+            $redirector->create($this->only(['name','permissions']))
+        );
+        $arrayIsExistsInTableRule->setExplodeResponseVisitor($responseVisitor);
         return [
             'name'=> 'required|max:64|unique:roles' ,
             'permissions' => ['array',$arrayIsExistsInTableRule] ,
