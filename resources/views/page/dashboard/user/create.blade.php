bd5a7808c9a4801a337d7aaa0bb089db034b6d60
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-23 19:23:29 +0200

    [file]{ create.blade.php } -> make view for create new user

---
+++ resources/views/page/dashboard/user/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+@section('dashboard-title')
+    <h1>create a new user </h1>
+@endsection
+@section('dashboard-content')
+    <p> create a new user is very funny </p>
+@endsection
