ea1ae35d9cea66caaf503156f34058308773932b
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-23 18:57:38 +0200

    [file]{ User.php } -> create redirector facade

---
+++ app/Facades/Redirectors/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Facades\Redirectors;
+use Illuminate\Support\Facades\Facade;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @see \App\Http\Redirector\User
+ * @method RedirectResponse viewAll()
+ */
+class User extends Facade
+{
+    protected static function getFacadeAccessor():string
+    {
+        return 'contract.redirector.user';
+    }
+}
