4257e4dedb7c4e4952b8ee7573609ea33f4e4693
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-23 12:52:51 +0200

    [file]{ Logger.php } -> modify used interface namespace

--- app/Contracts/Observers/Logger.php
+++ app/Contracts/Observers/Logger.php
@@ -3,9 +3,9 @@
 namespace App\Contracts\Observers;
 
 use App\Contracts\LoggerGetterable;
-use App\Contracts\Observers\Permission\Logger\Createdable;
-use App\Contracts\Observers\Permission\Logger\Deletedable;
-use App\Contracts\Observers\Permission\Logger\Updatedable;
+use App\Contracts\Observers\Logger\Createdable;
+use App\Contracts\Observers\Logger\Deletedable;
+use App\Contracts\Observers\Logger\Updatedable;
 use App\Contracts\TranslatorGetterable;
 
 interface Logger extends
