cbbd7d3779dd512d84376e71106dc39be3502149
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-23 19:24:34 +0200

    [file]{ CreateController.php } -> complete controller logic

--- app/Http/Controllers/Dashboard/User/CreateController.php
+++ app/Http/Controllers/Dashboard/User/CreateController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers\Dashboard\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CreateController extends Controller
 {
-    public function __invoke(
-
-    ):RedirectResponse|View
+    public function __invoke():RedirectResponse|View
     {
-        dd(__METHOD__);
+        try {
+            return \view('page.dashboard.user.create');
+        }catch (\Throwable $exception){
+            Log::emergency(
+                trans('log.crud.create.throw_exception', ['item' => 'user'])
+            );
+            report($exception);
+            $response = \UserRedirector::viewAll();
+            return \CreateActionResponseVisitor::throwException(
+                $response , 'user'
+            );
+        }
     }
 }
