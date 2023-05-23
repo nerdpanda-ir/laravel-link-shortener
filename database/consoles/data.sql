a8062b310323e61e4263f37096c378798bfe0c21
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-23 14:05:12 +0200

    [file]{ data.sql } -> add user view all permission to database

--- database/consoles/data.sql
+++ database/consoles/data.sql
@@ -8,7 +8,8 @@ insert into permissions values
                             (null , 'role-view-all'  , 1 , now() , null ) ,
                             (null , 'role-edit'  , 1 , now() , null ) ,
                             (null , 'role-delete'  , 1 , now() , null ),
-                            (null , 'role-create'  , 1 , now() , null );
+                            (null , 'role-create'  , 1 , now() , null ),
+                            (null , 'user-view-all'  , 1 , now() , null );
 insert into `permission_user` values
                                   (
                                       null , 1 , (select `id` from `permissions` where `permissions`.`name`='permission-view-all' ) , 1 , now() , null
@@ -30,4 +31,6 @@ insert into `permission_user` values
                                       null , 1 , (select `id` from `permissions` where `permissions`.`name`='role-delete' ) , 1 , now() , null
                                   ),(
                                       null , 1 , (select `id` from `permissions` where `permissions`.`name`='role-create' ) , 1 , now() , null
+                                  ),(
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='user-view-all' ) , 1 , now() , null
                                   );
