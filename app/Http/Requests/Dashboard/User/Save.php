89dcb0a635a4ae7213d69740d7b0a4f1ef89a1d0
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-25 09:16:54 +0200

    [file]{ save.php } -> update save request

--- app/Http/Requests/Dashboard/User/Save.php
+++ app/Http/Requests/Dashboard/User/Save.php
@@ -2,27 +2,47 @@
 
 namespace App\Http\Requests\Dashboard\User;
 
+use App\Contracts\Redirectors\User;
+use App\Contracts\Rule\ArrayIsExistsInTable;
+use App\Contracts\Services\ResponseVisitors\Rule\ExplodeArrayExistsInTable;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Contracts\Requests\Dashboard\User\Save as Contract;
+use App\Contracts\Services\MessageBuilders\Rule\Dashboard\User\ArrayIsExistsInTable as FailRuleMessageBuilder;
+use Illuminate\Contracts\Auth\Factory as Auth;
 class Save extends FormRequest implements Contract
 {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
+    public function authorize(Auth $auth): bool
     {
-        return false;
+        $user = $auth->guard('web')->user();
+        return $user->can('create-user');
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * @param \App\Http\Redirector\User $userRedirector
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     *
      */
-    public function rules(): array
+    public function rules(
+        User $userRedirector , ExplodeArrayExistsInTable $visitor , ArrayIsExistsInTable $arrayIsExistsInTableRule ,
+        FailRuleMessageBuilder $failRuleMessageBuilder ,
+    ): array
     {
+        $arrayIsExistsInTableRule->setExplodeResponse($userRedirector->viewAll());
+        $arrayIsExistsInTableRule->setExplodeResponseVisitor($visitor);
+        $arrayIsExistsInTableRule->setTable('roles');
+        $arrayIsExistsInTableRule->setColumn('name');
+        $arrayIsExistsInTableRule->setFailMessage($failRuleMessageBuilder);
         return [
-            //
+            'full_name'=> 'required|max:255' ,
+            'username' => 'required|max:42|unique:users,user_id' ,
+            'email' => 'required|max:42|email|unique:users,email|' ,
+            'password' => 'required|min:8|' ,
+            'password_confirmation' => 'required|min:8|confirmed' ,
+            'roles'=> $arrayIsExistsInTableRule ,
         ];
     }
 }
