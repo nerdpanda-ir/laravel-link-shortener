80f47d70eb8b71917c4b537e4643b1a26de231ff
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-25 10:12:42 +0200

    [file]{ Role.php } -> add dock block for facade

--- app/Facades/Models/Role.php
+++ app/Facades/Models/Role.php
@@ -3,6 +3,10 @@
 namespace App\Facades\Models;
 use Illuminate\Support\Facades\Facade;
 use App\Contracts\Model\Role as ModelContract;
+
+/**
+ * @see \App\Models\Role
+ */
 class Role extends Facade
 {
     protected static function getFacadeAccessor():string
