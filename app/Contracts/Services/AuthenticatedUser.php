304bc8501fb2d6eeeb41bac4032f2f1437508368
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-25 11:44:44 +0200

    [file]{ AuthenticatedUser.php } -> modify contract extend and create abstract method in the contract !!!

--- app/Contracts/Services/AuthenticatedUser.php
+++ app/Contracts/Services/AuthenticatedUser.php
@@ -2,9 +2,11 @@
 
 namespace App\Contracts\Services;
 
-use App\Contracts\Userable;
 
-interface AuthenticatedUser extends Userable
+use App\Contracts\Model\Userable;
+use App\Contracts\UserSetterable;
+
+interface AuthenticatedUser extends UserSetterable
 {
-
+    public function getUser():Userable|null;
 }
