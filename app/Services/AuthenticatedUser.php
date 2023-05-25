3493592d68bea13e4af18c72cb6cf009a160720a
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-25 12:21:18 +0200

    [file]{ AuthenticatedUser.php } -> modify used trait and declare user property and implement abstract method

--- app/Services/AuthenticatedUser.php
+++ app/Services/AuthenticatedUser.php
@@ -4,8 +4,14 @@ namespace App\Services;
 use App\Contracts\Services\AuthenticatedUser as Contract;
 use App\Traits\Userable;
 use App\Contracts\Model\Userable as Model;
+use App\Traits\UserSeeterable;
+
 class AuthenticatedUser implements Contract
 {
-    use Userable;
-    protected Model $user;
+    use UserSeeterable;
+    protected ?Model $user = null ;
+    public function getUser(): Model|null
+    {
+        return $this->user;
+    }
 }
