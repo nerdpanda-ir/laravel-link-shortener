558e6c5a940230477de8876e9a215e40984e7139
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-26 09:50:30 +0200

    [file]{  CreateController.php } -> update controller

--- app/Http/Controllers/Dashboard/User/CreateController.php
+++ app/Http/Controllers/Dashboard/User/CreateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Dashboard\User;
 
+use App\Contracts\Services\AuthenticatedUser;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Log;
@@ -9,10 +10,21 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CreateController extends Controller
 {
-    public function __invoke():RedirectResponse|View
+    public function __invoke(
+        AuthenticatedUser $authenticatedUserService
+    ):RedirectResponse|View
     {
         try {
-            return \view('page.dashboard.user.create');
+            $authenticatedUser = $authenticatedUserService->getUser();
+            $permissions = [
+                'can_attach_role_to_user' => $authenticatedUser->can('attach-role-to-user') ,
+                'can_set_password_for_user' => $authenticatedUser->can('set-password-for-user') ,
+                'can_verified_user_email' => $authenticatedUser->can('verified_user_email') ,
+            ];
+            $view  = \view('page.dashboard.user.create',$permissions);
+            if($permissions['can_attach_role_to_user'])
+                $view->with('roles',\RoleModel::all(['name']));
+            return $view;
         }catch (\Throwable $exception){
             Log::emergency(
                 trans('log.crud.create.throw_exception', ['item' => 'user'])
