503f145a0a43635790d6003d04ea7c6fa8cf0995
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-26 15:15:29 +0200

    [file]{  CouldHaveFillFields.php } -> compelete middleware

--- app/Http/Middleware/CouldHaveFillFields.php
+++ app/Http/Middleware/CouldHaveFillFields.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\HttpFoundation\Response;
 
 class CouldHaveFillFields
@@ -13,8 +15,24 @@ class CouldHaveFillFields
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(
+        Request $request, Closure $next , string $fields ,
+        string $permissions
+    ): Response
     {
-        return $next($request);
+        $shouldNoFilledFields = [];
+        /** @var User $user*/
+        $user = \AuthenticatedUser::getUser();
+        $fieldsArray = explode('|',trim($fields));
+        $permissionsArray = explode('|',trim($permissions));
+        foreach ($fieldsArray as $fieldKey=> $fieldItem)
+            if($request->query->has($fieldItem) && $user->cant($permissionsArray[$fieldKey]))
+                $shouldNoFilledFields[] = $fieldItem;
+        if (empty($shouldNoFilledFields))
+            return $next($request);
+        $errors = [];
+        foreach ($shouldNoFilledFields as $shouldNoFilledField)
+            $errors[] = "you cant fill $shouldNoFilledField because you`re no authorize for this action !!!";
+        return Redirect::back()->with('system.messages.error',$errors);
     }
 }
