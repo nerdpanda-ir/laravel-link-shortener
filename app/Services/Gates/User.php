6e940370ab29a73d653fa8aab8ec5fb4cbc2f893
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-27 07:55:19 +0200

    [files]{  User.php } -> implement abstract method

--- app/Services/Gates/User.php
+++ app/Services/Gates/User.php
@@ -7,4 +7,10 @@ use App\Traits\Services\Gates\User as UserTrait;
 class User extends AbstractCrudableGate implements Contract
 {
     use UserTrait;
+
+    public function setPasswordForUser(): bool
+    {
+        return $this->getPermissionManager()->has('set-password-for-user');
+    }
+
 }
