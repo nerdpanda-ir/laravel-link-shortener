149314635d0b7fd0fb6bd9a12733cff2ca66b848
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-27 15:56:03 +0200

    [file]{ edit.blade.php } -> create view for edit user

---
+++ resources/views/page/dashboard/user/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+@section('dashboard-title')
+    <h1> Edit User {{ $user->name }}</h1>
+@endsection
+@section('dashboard-content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt dolorem ea earum enim in magnam, mollitia nihil possimus provident quis quos vitae, voluptate. Aut distinctio explicabo molestiae nulla similique.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt dolorem ea earum enim in magnam, mollitia nihil possimus provident quis quos vitae, voluptate. Aut distinctio explicabo molestiae nulla similique.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt dolorem ea earum enim in magnam, mollitia nihil possimus provident quis quos vitae, voluptate. Aut distinctio explicabo molestiae nulla similique.</p>
+@endsection
