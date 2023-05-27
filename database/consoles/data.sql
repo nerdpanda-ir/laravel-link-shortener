e3eca7a4296f454d65faa71367d6d5f751708c69
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-27 11:14:12 +0200

    [files]{  Data.sql  } -> insert data to database

--- database/consoles/data.sql
+++ database/consoles/data.sql
@@ -11,6 +11,8 @@ insert into permissions values
                             (null , 'role-create'  , 1 , now() , null ),
                             (null , 'user-view-all'  , 1 , now() , null ) ,
                             (null , 'user-create'  , 1 , now() , null ),
+                            (null , 'user-edit'  , 1 , now() , null ),
+                            (null , 'user-delete'  , 1 , now() , null ),
                             (null , 'set-password-for-user'  , 1 , now() , null ),
                             (null , 'attach-role-to-user'  , 1 , now() , null ),
                             (null , 'verify-user-email'  , 1 , now() , null );
@@ -39,6 +41,10 @@ insert into `permission_user` values
                                       null , 1 , (select `id` from `permissions` where `permissions`.`name`='user-view-all' ) , 1 , now() , null
                                   ),(
                                       null , 1 , (select `id` from `permissions` where `permissions`.`name`='user-create' ) , 1 , now() , null
+                                  ),(
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='user-edit' ) , 1 , now() , null
+                                  ),(
+                                      null , 1 , (select `id` from `permissions` where `permissions`.`name`='user-delete' ) , 1 , now() , null
                                   ),(
                                       null , 1 , (select `id` from `permissions` where `permissions`.`name`='set-password-for-user' ) , 1 , now() , null
                                   ),(
