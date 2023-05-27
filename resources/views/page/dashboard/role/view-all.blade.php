cc7a3e8282d0b138a5175a21b9354b3438adc619
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-27 07:49:57 +0200

    [files]{  permissions/view-all.blade.php ,  roles/view-all.blade.php } -> remove duplicated code and replace with component !!!

--- resources/views/page/dashboard/role/view-all.blade.php
+++ resources/views/page/dashboard/role/view-all.blade.php
@@ -32,14 +32,7 @@
                         <td>{{$loop->iteration}}</td>
                         <td>{{$role->name}}</td>
                         <td>
-                            @unless(is_null($role->creator))
-                                {{$role->creator->name}}
-                            @else
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash text-danger" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                                </svg>
-                            @endunless
+                            <x-partials.creator-name-printer-proxy :creator="$role->creator" />
                         </td>
                         <td><a href="">{{$role->users_count}}</a></td>
                         <td><a href="">{{$role->permissions_count}}</a></td>
