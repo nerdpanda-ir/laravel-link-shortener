86d44691762c84014cafb9604b16c0e2fc859f9d
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-28 10:01:27 +0200

    [file]{ Update.php } -> form request implement contract

--- app/Http/Requests/Dashboard/User/Update.php
+++ app/Http/Requests/Dashboard/User/Update.php
@@ -3,8 +3,8 @@
 namespace App\Http\Requests\Dashboard\User;
 
 use Illuminate\Foundation\Http\FormRequest;
-
-class Update extends FormRequest
+use App\Contracts\Requests\Dashboard\User\Update as Contract;
+class Update extends FormRequest implements Contract
 {
     /**
      * Determine if the user is authorized to make this request.
