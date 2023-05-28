6fd98573914c6fb56abe88529ad4ca16a0dc8f70
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-28 18:51:21 +0200

    [file]{ UpdateAction.php } -> add dock block for facade

--- app/Facades/Services/ResponseVisitors/UpdateAction.php
+++ app/Facades/Services/ResponseVisitors/UpdateAction.php
@@ -4,6 +4,10 @@ namespace App\Facades\Services\ResponseVisitors;
 
 use Illuminate\Support\Facades\Facade;
 use App\Contracts\Services\ResponseVisitors\UpdateAction as Accessor;
+
+/**
+ * @see \App\Services\ResponseVisitors\UpdateAction
+ */
 class UpdateAction extends Facade
 {
     protected static function getFacadeAccessor():string
