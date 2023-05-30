df32058b397821f4a4852d95fa1ef392226a437a
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-30 13:23:56 +0200

    [file]{ CantAttachRoleToUser.php } => remove extend from any interface

--- app/Contracts/Services/Strategies/UserUpdate/CantAttachRoleToUser.php
+++ app/Contracts/Services/Strategies/UserUpdate/CantAttachRoleToUser.php
@@ -2,9 +2,8 @@
 
 namespace App\Contracts\Services\Strategies\UserUpdate;
 
-use App\Contracts\Services\UserUpdateStrategy;
 
-interface CantAttachRoleToUser extends UserUpdateStrategy
+interface CantAttachRoleToUser
 {
 
 }
