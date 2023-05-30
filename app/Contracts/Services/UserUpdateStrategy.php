2790bd744bdd64d364d6f2f7c084813098ca2c41
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-30 15:37:32 +0200

    [file]{ UserUpdateStrategy.php }-> remove extend

--- app/Contracts/Services/UserUpdateStrategy.php
+++ app/Contracts/Services/UserUpdateStrategy.php
@@ -5,7 +5,7 @@ namespace App\Contracts\Services;
 use App\Contracts\RequestGetterable;
 use App\Contracts\Userable;
 
-interface UserUpdateStrategy extends Userable , RequestGetterable
+interface UserUpdateStrategy extends Userable
 {
     public function updateCommand():bool;
 }
