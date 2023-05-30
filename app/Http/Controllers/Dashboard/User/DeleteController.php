827a03258ee5926fbd35816072d1813fea71bf54
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-30 17:05:16 +0200

    [file] { DeleteController.php } => complete controller

--- app/Http/Controllers/Dashboard/User/DeleteController.php
+++ app/Http/Controllers/Dashboard/User/DeleteController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers\Dashboard\User;
 
+use App\Exceptions\FailCrud;
 use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Contracts\Exceptions\FailCrud as FailCrudContract;
 class DeleteController extends Controller
 {
     public function __invoke(string $id , string $name):RedirectResponse
     {
-        dd($id , $name);
+        try {
+            $user = \UserModel::find($id,['id','name']);
+            if (!$user)
+                throw new NotFoundHttpException();
+            $deleted = $user->delete();
+            if (!$deleted)
+                throw new FailCrud();
+            return \DeleteActionResponseVisitor::ok(
+                \UserRedirector::viewAll() , 'user with name '.$user->name
+            );
+        }catch (NotFoundHttpException $exception){
+            return \NotFoundResponseVisitor::visit (\UserRedirector::viewAll() , "user $name");
+        }catch (FailCrudContract $exception){
+            $exception->setMessage(trans('log.crud.delete.fail', ['item' => 'user']));
+            $exception->setContext(['user'=>$user]);
+            report($exception);
+            return  \DeleteActionResponseVisitor::fail(
+                \UserRedirector::viewAll() , "user with name $user->name"
+            );
+        }
     }
 }
