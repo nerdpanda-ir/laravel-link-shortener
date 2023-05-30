f53494d445beb90cfd673dc2d69763e11c818e0a
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-30 13:07:21 +0200

    [file]{ UserUpdateStrategy.php } => create abstract class

---
+++ app/Services/UserUpdateStrategy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+use App\Contracts\Services\UserUpdateStrategy as Contract;
+use App\Exceptions\FailCrud;
+use App\Traits\RequestGetterable;
+use App\Traits\Userable;
+use App\Contracts\Model\Userable as UserModel;
+use Illuminate\Http\Request;
+
+abstract class UserUpdateStrategy implements Contract
+{
+    use Userable , RequestGetterable ;
+    protected UserModel $user;
+    protected Request $request;
+    protected function updateUser():bool {
+        $updated = $this->getUser()->update();
+        if (!$updated)
+            throw new FailCrud();
+        return $updated;
+    }
+}
