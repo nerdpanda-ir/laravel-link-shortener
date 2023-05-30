33971fa5655d4dd870455becf0b6104340c718b9
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-30 15:29:57 +0200

    [file]{ app.php } => register CantAttachRoleToUserServiceProvider into container

--- config/app.php
+++ config/app.php
@@ -221,6 +221,7 @@ return [
         \App\Providers\Services\MessageBuilders\Rule\Dashboard\User\ArrayIsExitstsInTableServiceProvider::class ,
         \App\Providers\Services\AuthenticatedUserServiceProvider::class ,
         \App\Providers\Request\Dashboard\User\UpdateServiceProvider::class ,
+        \App\Providers\Services\Strategies\UserUpdate\CantAttachRoleToUserServiceProvider::class ,
     ])->toArray(),
 
     /*
