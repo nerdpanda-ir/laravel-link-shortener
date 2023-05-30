f056265d5cd6ba60bc41f6cdcbad7449190ba8ab
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-30 11:04:07 +0200

    [file]{ UpdateController.php } -> complete controller logic

--- app/Http/Controllers/Dashboard/User/UpdateController.php
+++ app/Http/Controllers/Dashboard/User/UpdateController.php
@@ -2,13 +2,98 @@
 
 namespace App\Http\Controllers\Dashboard\User;
 
+use App\Exceptions\FailCrud;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Contracts\Requests\Dashboard\User\Update as Request;
+use App\Contracts\Exceptions\FailCrud as FailCrudContract;
 class UpdateController extends Controller
 {
-    public function __invoke(string $id , string $name):RedirectResponse
-    {
 
+    /**
+     * @param string $id
+     * @param string $name
+     * @param \Illuminate\Http\Request $request
+     * @return RedirectResponse
+     */
+    public function __invoke(string $id , string $name , Request $request):RedirectResponse
+    {
+        try {
+            /** @var User $user */
+            $user = \UserModel::find($id,['name','id','email_verified_at']);
+            if (is_null($user))
+                throw new NotFoundHttpException();
+            $now = \DateServiceFacade::date();
+            /** @var User $authenticatedUser */
+            $authenticatedUser = \AuthenticatedUser::getUser();
+            $hasEmailVerifiedInput = $request->has('email_verified');
+            $userRealName = $user->name;
+
+            $data = [
+                'name'=> $request->get('full_name') , 'user_id' => $request->get('username') ,
+                'email' => $request->get('email') , 'updated_at' => $now , 'id'=> $user->id ,
+            ];
+            if ($request->has('password') and !is_null($request->get('password')))
+                $data['password'] = Hash::make($request->get('password'));
+
+            if($authenticatedUser->can('verify-user-email') && !$hasEmailVerifiedInput &&
+                !is_null($user->email_verified_at))
+                    $data['email_verified_at'] = null;
+            elseif($hasEmailVerifiedInput && is_null($user->email_verified_at))
+                    $data['email_verified_at'] = $now;
+
+            $user->setRawAttributes($data);
+            $canAttachRoleToUser = $authenticatedUser->can('attach-role-to-user');
+            DB::beginTransaction();
+                $updated = $user->update();
+                if (!$updated)
+                    throw new FailCrud();
+                if ($canAttachRoleToUser) {
+                    $roles = [];
+                    if ($request->has('roles')) {
+                        $roles = \RoleModel::whereIn('name',$request->get('roles'))->get(['id']);
+                        $roles = $roles->pluck('id')->toArray();
+                        $rolePayload = ['created_at'=> $now , 'created_by'=> $authenticatedUser->id];
+                        $roles = array_map(fn(int $roleId)=> array_merge($rolePayload,['role_id'=>$roleId]) , $roles );
+                    }
+                    $user->roles()->sync($roles);
+                }
+            DB::commit();
+            return \UpdateActionResponseVisitor::ok(\UserRedirector::viewAll(),"user $name");
+        }catch (NotFoundHttpException $exception){
+            return \NotFoundResponseVisitor::visit(
+                \UserRedirector::viewAll(),"user with id $id and with name $name"
+            );
+        }catch (FailCrudContract $exception){
+            $exception->setMessage(trans('log.crud.updated.fail', ['item' => 'user']));
+            $exceptionContext = ['user'=> $user];
+            if ($canAttachRoleToUser)
+                $exceptionContext['roles'] = (($request->has('roles')) ? $request->get('roles') : []);
+            $exception->setContext($exceptionContext);
+            report($exception);
+            return \UpdateActionResponseVisitor::fail(
+                \UserRedirector::edit($id,$name,$request->except(['_token','_method'])) ,
+                "user with name $userRealName"
+            );
+        }catch (\Throwable $exception){
+            // log ->
+            Log::emergency(
+                trans('log.crud.updated.throw_exception', ['item' => 'user']),
+                ((isset($user)) ? ['user'=> $user] : ['id'=>$id , 'name'=> $name ])
+            );
+            //report exception
+            report($exception);
+            // redirect
+            return \UpdateActionResponseVisitor::throwException(
+                \UserRedirector::edit($id,$name,$request->except(['_token','_method'])) ,
+                'user '.($user->name ?? $name)
+            );
+
+        }
     }
 }
