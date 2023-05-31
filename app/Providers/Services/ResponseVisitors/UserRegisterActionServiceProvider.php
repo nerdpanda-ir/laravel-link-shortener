3871ac529ac2a1d0dd51b38ea5b130533fb774ac
NerdPanda <nerdpandabusiness@gmail.com>
2023-05-31 12:29:14 +0200

    [file] { UserRegisterActionServiceProvider.php } => modify provider

--- app/Providers/Services/ResponseVisitors/UserRegisterActionServiceProvider.php
+++ app/Providers/Services/ResponseVisitors/UserRegisterActionServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers\Services\ResponseVisitors;
 
-use App\Contracts\Redirectors\Home;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 use App\Contracts\Services\ResponseVisitors\UserRegisterAction as Contract;
 use App\Services\ResponseVisitors\UserRegisterAction as ResponseVisitor;
@@ -14,19 +11,6 @@ class UserRegisterActionServiceProvider extends ServiceProvider implements Defer
     public function register(): void
     {
         $this->app->bind(Contract::class,ResponseVisitor::class);
-        $this->app->afterResolving(
-            Contract::class ,
-            function (Contract $responseVisitor , Application $container)
-            {
-                /** @var Request $request */
-                $request = $container->get('request');
-                /** @var \App\Http\Redirector\Home $request */
-                $redirector = $container->get(Home::class);
-
-                $responseVisitor->setUserfullName($request->get('full_name'));
-                $responseVisitor->setRedirectResponse($redirector->redirect());
-            }
-        );
     }
     public function provides():array
     {
