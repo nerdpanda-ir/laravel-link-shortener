74a24f304f5284148868f10d3f9f75e3976da950
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-01 14:55:38 +0200

    [file] { UserPermissionsRepositoryImpl.php } => create UserPermissionsRepositoryImpl service

---
+++ app/Services/UserPermissionsRepositoryImpl.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+use App\Services\UserPermissionsRepositoryAbstract as Base ;
+use App\Contracts\Services\UserPermissionsRepositoryImpl as Contract;
+class UserPermissionsRepositoryImpl extends Base implements Contract
+{
+
+}
