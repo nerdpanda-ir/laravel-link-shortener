f0446ebbf2d6bbe8fa77ae6df3fbd9516a88b30a
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-02 10:53:30 +0200

    [file] { LazyPermissionManagerStrategyImp.php } => create service

---
+++ app/Services/LazyPermissionManagerStrategyImp.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+use App\Services\LazyPermissionManagerStrategy as Base;
+use App\Contracts\Services\LazyPermissionManagerStrategyImp as Contract;
+class LazyPermissionManagerStrategyImp extends Base implements Contract
+{
+
+}
