c1eee4711a25f799a5d94460bcbbbb1182c97d73
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-02 12:09:50 +0200

    [file] { PermissionManagerServiceProvider.php } => modify provider

--- app/Providers/Services/PermissionManagerServiceProvider.php
+++ app/Providers/Services/PermissionManagerServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers\Services;
 
+use App\Contracts\Services\AuthenticatedUser;
 use App\Contracts\Services\PermissionManager as Contract;
-use App\Services\PermissionManagerService as Service;
+use App\Services\LazyPermissionManagerStrategyImp as Service;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,6 +14,14 @@ class PermissionManagerServiceProvider extends ServiceProvider implements Deferr
     public function register(): void
     {
         $this->app->singleton(Contract::class,Service::class);
+        $this->app->afterResolving(
+            Contract::class,
+            function (Contract $permissionManager , Application $container){
+                $user = $container->get(AuthenticatedUser::class)->getUser();
+                if (!is_null($user))
+                    $permissionManager->setUser($user);
+            }
+        );
     }
     public function provides():array
     {
