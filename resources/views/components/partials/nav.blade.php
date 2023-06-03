a87f311d8bf17564cb7aacb3106f04a98c3adc1b
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 10:17:49 +0200

    [file] { nav.blade.php } => add create link into navbar

--- resources/views/components/partials/nav.blade.php
+++ resources/views/components/partials/nav.blade.php
@@ -10,6 +10,9 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('link.create')}}">Create Link</a>
+                </li>
                 @guest
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('login')}}">login</a>
