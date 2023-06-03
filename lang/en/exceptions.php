9d02ce2ec8c33e656309367684898e3f547c1474
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 14:19:12 +0200

    [file] { exceptions.php } => create translate item

--- lang/en/exceptions.php
+++ lang/en/exceptions.php
@@ -2,4 +2,5 @@
 return [
     'no-dependency-found' => 'number of :dependency is not enough for seeding :seeder ' ,
     'fail_crud' => 'has exception when :action ' ,
+    'link_summary_maximum_try' => 'maximum try for generate link summary happened please add more length for link summary  or increase maximum try for generate link summary because i should find unique case for link !!!'
 ];
