b777cd234970ae1e862a7390a80d81279f6fed25
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 18:14:24 +0200

    [file] { frontPage.blade.php } => display system error

--- resources/views/layouts/frontPage.blade.php
+++ resources/views/layouts/frontPage.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.bootstrap')
 @section('content')
     <x-partials.nav />
+    @if(session()->has('system.messages.error') || session()->has('system.messages.ok'))
+        <section class="mt-5 mb-5 container">
+            <x-partials.system-messages-printer />
+        </section>
+    @endif
 @endsection
