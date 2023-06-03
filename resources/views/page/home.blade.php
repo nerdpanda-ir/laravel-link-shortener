141c5147cb261cf25298153cf3456ad111d9f300
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 18:17:12 +0200

    [file] { home.blade.php } => remove show system errors component

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -5,7 +5,6 @@
 @section('content')
 @parent
 <main class="container py-5">
-    <x-partials.system-messages-printer />
     <h1>Link Shortener</h1>
     <p>you can short your links with me !!!</p>
     <hr class="my-5">
