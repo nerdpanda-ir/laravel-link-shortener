f645403466f60c5458e22968148a6dd95838ac71
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 11:12:01 +0200

    [file] { create.blade.php } => add save url into form action

--- resources/views/page/links/create.blade.php
+++ resources/views/page/links/create.blade.php
@@ -8,8 +8,9 @@
         <section class="container">
             <h1>CREATE A NEW LINK</h1>
             <x-partials.form-error-printer />
-            <form class="needs-validation mt-5" novalidate="" action=""
+            <form class="needs-validation mt-5" novalidate="" action="{{route('link.save')}}"
                   method="post">
+                @csrf
                 <x-partials.field-error-printer name="url"/>
                 <div class="col-sm-10 d-inline-block mx-4">
                     <label for="url" class="form-label">Links : </label>
