a816290d4db1e7870baf02c5e0154cdf398e0043
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 11:30:34 +0200

    [file] { form-error-printer.blade.php } => DYNAMIC CLASS ATTRIBUTE FOR COMPONENT

--- resources/views/components/partials/form-error-printer.blade.php
+++ resources/views/components/partials/form-error-printer.blade.php
@@ -1,5 +1,5 @@
 @if($errors->any())
-    <x-alert class="p-4 bg-dark text-danger" style="font-size: 16px;font-weight: bold">
+    <x-alert {{$attributes->class('p-4 bg-dark text-danger')}} style="font-size: 16px;font-weight: bold">
         <x-slot name="message">
             <ul class="m-0">
                 @foreach($errors->all() as $errorItem)
