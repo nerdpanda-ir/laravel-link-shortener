4e51c7a3db7c4471b719d17bd730a1dbb7ff268b
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 06:21:19 +0200

    [file] { 2023_06_02_154814_create_links_table.php } => create migration

---
+++ database/migrations/2023_06_02_154814_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('original',2000)->index();
+            $table->string('summary')->unique();
+            $table->unsignedBigInteger('creator')->nullable();
+            $table->timestamps();
+
+            $table->foreign('creator')
+                  ->on('users')->references('id')
+                  ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
