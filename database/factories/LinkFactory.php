31ddaa332582961389ccc1586296d293fafdd036
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-03 06:26:16 +0200

    [file] { LinkFactory.php } => create factory

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Contracts\Factories\Link as Contract;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Link>
+ */
+class LinkFactory extends Factory implements Contract
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
