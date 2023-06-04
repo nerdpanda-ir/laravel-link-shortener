ed1f9d8fec1760fad6f1eb041a4ba3cf47503a43
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-04 15:20:00 +0200

    [file] { create.blade.php } => delete form and use from component

--- resources/views/page/links/create.blade.php
+++ resources/views/page/links/create.blade.php
@@ -8,17 +8,7 @@
         <section class="container">
             <h1>CREATE A NEW LINK</h1>
             <x-partials.form-error-printer class="mt-5 mb-5"/>
-            <form class="needs-validation mt-5" novalidate="" action="{{route('link.save')}}"
-                  method="post">
-                @csrf
-                <div class="col-sm-10 d-inline-block mx-4">
-                    <label for="url" class="form-label">Links : </label>
-                    <input type="text" class="form-control" id="url" placeholder="https://google.com"
-                           value="{{old('url')}}" name="url">
-                </div>
-                <button class="btn btn-primary position-relative" type="submit" style="bottom: 4px"> Generate </button>
-                <x-partials.field-error-printer name="url" class="mt-2 mx-4"/>
-            </form>
+            <x-partials.create-link-form action="{{route('link.save')}}" />
         </section>
     </main>
 @endsection
