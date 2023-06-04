4f0e661de0f2eb6e474b621cea8c357c0eee0a8f
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-04 15:07:11 +0200

    [file] { CreateController.php } => create controller

---
+++ app/Http/Controllers/Dashboard/Link/CreateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Link;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+
+class CreateController extends Controller
+{
+    public function __invoke():View
+    {
+        dd("hellow");
+    }
+}
