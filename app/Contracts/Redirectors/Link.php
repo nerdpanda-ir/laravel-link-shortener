22a38cfc5ee82d1080def324f41027f32a3324d7
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-04 07:36:19 +0200

    [file] { Link.php } => create abstract method in contract

--- app/Contracts/Redirectors/Link.php
+++ app/Contracts/Redirectors/Link.php
@@ -2,7 +2,9 @@
 
 namespace App\Contracts\Redirectors;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 interface Link extends Createable
 {
-
+    public function show(string $summary):RedirectResponse;
 }
