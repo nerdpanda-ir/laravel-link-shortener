2e98cb15720814868959197aa6aa5be46f0c1500
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-05 09:00:59 +0200

    [file] { Link.php } => create contract

---
+++ app/Contracts/Redirectors/Dashboard/Link.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Contracts\Redirectors\Dashboard;
+
+use App\Contracts\Redirectors\Createable;
+use App\Contracts\Redirectors\ViewAllable;
+
+interface Link extends Createable , ViewAllable
+{
+
+}
