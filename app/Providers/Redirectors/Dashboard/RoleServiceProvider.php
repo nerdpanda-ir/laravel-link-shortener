e7e427c0ae6fe77496d186a8af20bbf85a655475
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-05 11:37:30 +0200

    [file]{RoleServiceProvider.php}-> move to dashboard directory

--- app/Providers/Redirectors/Dashboard/RoleServiceProvider.php
+++ app/Providers/Redirectors/Dashboard/RoleServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers\Redirectors;
+namespace App\Providers\Redirectors\Dashboard;
 
 use App\Contracts\Redirectors\Dashboard\Role as Contract;
 use App\Http\Redirector\Dashboard\Role as Redirector;
