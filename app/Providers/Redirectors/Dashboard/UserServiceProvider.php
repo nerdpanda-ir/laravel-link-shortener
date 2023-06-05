2d8c470bc470509c41bda35a7cd85918ec2eeaaf
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-05 12:24:36 +0200

    [file] { UserServiceProvider.php } => move to dashboard directory

--- app/Providers/Redirectors/Dashboard/UserServiceProvider.php
+++ app/Providers/Redirectors/Dashboard/UserServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers\Redirectors;
+namespace App\Providers\Redirectors\Dashboard;
 
 use App\Contracts\Redirectors\Dashboard\User as Contract;
 use App\Http\Redirector\Dashboard\User as Redirector;
