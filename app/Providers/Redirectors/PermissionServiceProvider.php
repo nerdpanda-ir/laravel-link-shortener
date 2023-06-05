186abedca11e3d908c3422a389ea3c6fc87bbff2
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-05 10:44:17 +0200

    modify permission redirector namespace

--- app/Providers/Redirectors/PermissionServiceProvider.php
+++ app/Providers/Redirectors/PermissionServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers\Redirectors;
 
 use App\Contracts\Redirectors\Dashboard\Permission as Contract;
-use App\Http\Redirector\Permission as Redirector;
+use App\Http\Redirector\Dashboard\Permission as Redirector;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
