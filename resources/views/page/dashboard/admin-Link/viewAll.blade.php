f059590f9f74a0daf5a35a55dd26becdae32da64
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-05 17:59:31 +0200

    [file] {  viewAll.blade.php } -> create view

---
+++ resources/views/page/dashboard/admin-Link/viewAll.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+@section('dashboard-title')
+    <h1>view links</h1>
+@endsection
+@section('dashboard-content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi aperiam atque cumque cupiditate, delectus dicta dolores eaque ex incidunt iusto labore nostrum perspiciatis placeat provident ullam vel! Maiores, vel? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deleniti esse et inventore laborum non praesentium repudiandae? At commodi ducimus est explicabo impedit minima tempora voluptates! Blanditiis itaque necessitatibus quasi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus exercitationem illo illum nostrum officia? Accusantium ad, ea eaque, eligendi facere illum nemo, placeat porro provident quibusdam saepe vel? Asperiores, ipsum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi deleniti doloribus illum inventore laudantium natus, nisi obcaecati optio temporibus voluptatibus. Animi aperiam illum molestias, nihil pariatur quod reprehenderit rerum tempore?</p>
+@endsection
