bbbfb14387f890d6e552f3531f735534f404d634
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-06 15:28:19 +0200

    [file] { DeleteController.php } => complete controller

--- app/Http/Controllers/Dashboard/AdminLink/DeleteController.php
+++ app/Http/Controllers/Dashboard/AdminLink/DeleteController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Dashboard\AdminLink;
 
 use App\Contracts\Exceptions\FailCrud as FailCrudContract;
 use App\Contracts\Model\Link;
+use App\Contracts\Redirectors\Dashboard\AdminLink as Redirector;
+use App\Contracts\Services\ResponseVisitors\DeleteAction;
+use App\Contracts\Services\ResponseVisitors\NotFound;
 use App\Exceptions\FailCrud;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface as Logger;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -17,15 +23,44 @@ class DeleteController extends Controller
      * @param \App\Models\Link $link
      * @return RedirectResponse
      */
-    public function __invoke(string $id , Request $request , Link $linkModel , ):RedirectResponse
+    public function __invoke(
+        string $id , Request $request , Link $linkModel , Redirector $redirector , NotFound $notFoundVisitor ,
+        Translator $translator , ExceptionHandler $exceptionHandler , DeleteAction $responseVisitor ,
+        Logger $logger ,
+    ):RedirectResponse
     {
         try {
-            dd(__METHOD__);
-            #$link = $linkModel->where('id','=',$id)->first(['id','original']);
+            $link = $linkModel->where('id','=',$id)->first(['id','original']);
+            if (is_null($link))
+                throw new NotFoundHttpException();
+            $deleted = $link->delete();
+            if (!$deleted)
+                throw new FailCrud();
+            return $responseVisitor->ok(
+                $redirector->viewAll(),"link with id => $id and with url => {$link->original}"
+            );
         }catch (NotFoundHttpException $exception){
-            dd($exception);
+            return $notFoundVisitor->visit(
+                $redirector->viewAll() , "link with id : $id and url -> {$request->get('link')}"
+            );
         }catch (FailCrudContract $exception){
-            dd($exception);
-        }catch (\Throwable $exception){}
+            $exception->setMessage($translator->get('log.crud.delete.fail', ['item' => 'link']));
+            $exception->setContext(['link'=> $link ]);
+            $exceptionHandler->report($exception);
+            return $responseVisitor->fail(
+                $redirector->viewAll() , "link with id => $id and with url => {$link->original}"
+            );
+        }catch (\Throwable $exception){
+            $hasLinkModel = isset($link);
+            $logger->emergency(
+                $translator->get('log.crud.delete.throw_exception', ['item' => 'link']) ,
+                ['link'=> ( ( $hasLinkModel ) ? $link : ['id'=> $id , 'url'=> $request->get('link')] )]
+            );
+            $exceptionHandler->report($exception);
+            return $responseVisitor->throwException(
+                $redirector->viewAll(),
+                " link with id -> $id and url -> ".( ( $hasLinkModel ) ? $link->original : $request->get('link') )
+            );
+        }
     }
 }
