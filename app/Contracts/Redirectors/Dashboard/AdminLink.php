2414b15a19007f604afda0348d31d97e016452a3
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-06 08:25:51 +0200

    [file] { AdminLink.php } -> create contract

---
+++ app/Contracts/Redirectors/Dashboard/AdminLink.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Contracts\Redirectors\Dashboard;
+
+use App\Contracts\Redirectors\Editable;
+use App\Contracts\Redirectors\ViewAllable;
+
+interface AdminLink extends ViewAllable , Editable
+{
+
+}
