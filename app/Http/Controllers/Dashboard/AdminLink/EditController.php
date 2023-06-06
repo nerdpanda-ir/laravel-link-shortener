14fcd9cb12eabcfaa90f431a72caa30959a716bf
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-06 08:24:19 +0200

    [file] { EditController.php } -> modify controller parameter

--- app/Http/Controllers/Dashboard/AdminLink/EditController.php
+++ app/Http/Controllers/Dashboard/AdminLink/EditController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Dashboard\AdminLink;
 use App\Http\Controllers\Controller;
 use http\Encoding\Stream\Inflate;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class EditController extends Controller
 {
     public function __invoke(
-        string $id , string $link ,
+        string $id , Request $request
     ):View|RedirectResponse
     {
         try {
