279005a187f4a4ff0a61646538e56868bfc7630f
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-06 09:16:42 +0200

    [file] { EditController.php } -> complete edit controller

--- app/Http/Controllers/Dashboard/AdminLink/EditController.php
+++ app/Http/Controllers/Dashboard/AdminLink/EditController.php
@@ -2,22 +2,54 @@
 
 namespace App\Http\Controllers\Dashboard\AdminLink;
 
+use App\Contracts\Model\Link;
+use App\Contracts\Redirectors\Dashboard\AdminLink;
+use App\Contracts\Redirectors\Dashboard\AdminLink as Redirector;
+use App\Contracts\Services\ResponseVisitors\EditAction as EditVisitor;
+use App\Contracts\Services\ResponseVisitors\NotFound as NotFoundVisitor;
 use App\Http\Controllers\Controller;
 use http\Encoding\Stream\Inflate;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface as Logger;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class EditController extends Controller
 {
+
+    /**
+     * @param \App\Models\Link $linkModel
+     * @return View|RedirectResponse
+     * @throws \Throwable
+     */
     public function __invoke(
-        string $id , Request $request
+        string $id , Request $request , Logger $logger , Translator $translator , ExceptionHandler $exceptionHandler ,
+        EditVisitor $editVisitor , Redirector $redirector , NotFoundVisitor $notFoundVisitor , Link $linkModel ,
     ):View|RedirectResponse
     {
         try {
-            dd("here");
+            $link = $linkModel->where('id','=',$id)->first(['id','original','summary','view_count']);
+            if (is_null($link))
+                throw new NotFoundHttpException();
+            return \view('page.dashboard.admin-Link.edit',compact('link'));
+        }catch (NotFoundHttpException $exception){
+            return $notFoundVisitor->visit(
+                $redirector->viewAll(),
+                "link with id : $id and url : {$request->get('link')}"
+            );
         }catch (\Throwable $exception){
-            dd($exception);
+            $url = $request->get('link',"");
+            $logger->emergency(
+                $translator->get('log.crud.edit.throw_exception', ['item' => 'link']) ,
+                ['link' => (( isset($link) ) ? $link : ['id'=> $id , 'url'=> $url ])]
+            );
+            $exceptionHandler->report($exception);
+            return $editVisitor->throwException(
+                $redirector->viewAll(), "link with id -> $id and url -> $url"
+            );
         }
     }
 }
