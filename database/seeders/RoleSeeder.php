fd62be4f670960360d44e893599e71eeb9e0efda
NerdPanda <nerdpandabusiness@gmail.com>
2023-06-07 05:41:08 +0200

    [file] { RoleSeeder.php } => modify seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Contracts\Factories\Role;
 use App\Contracts\Model\Userable;
 use App\Contracts\Seeder\Role as Contract;
+use App\Facades\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 use NerdPanda\Exceptions\NoDependencyFoundForSeedingException;
@@ -17,14 +18,12 @@ class RoleSeeder extends Seeder implements Contract
     public function run(Role $factory , Userable $user): void
     {
         $factory->newModel()->truncate();
-        /** @var Collection $users*/
-        $users = $user->whereVerifiedEmail()->offset(0)->limit(3)->get(['id']);
-        if ($users->isEmpty() xor $users->count()==1)
+        /** @var User $root_user*/
+        $root_user = $user->where('id','=','1')->first(['id']);
+        if (is_null($root_user))
             throw new NoDependencyFoundForSeedingException(trans(
-                'exceptions.no-dependency-found', ['dependency' => 'users', 'seeder' => self::class]
+                'exceptions.no-dependency-found', ['dependency' => 'root user', 'seeder' => self::class]
             ));
-        $roles = ['root','admin','manager','author','user'];
-        foreach ($roles as $role)
-            $factory->create(['created_by'=> $users->random(), 'name'=>$role]);
+        $factory->create(['created_by'=> $root_user->id, 'name'=>'root']);
     }
 }
